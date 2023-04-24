ded74f16a5e0e6a9928dcdb132132382d30ae9b6
Fino59 <slipdan@hotmail.com>
2023-04-24 18:51:36 -0300

    Update Layout component

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -37,7 +37,7 @@ export default function Layout() {
             // },
             {
               to: '/servicos',
-              name: 'Serviços',
+              name: 'Produtos',
               icon: IconClipboard
             },
             {
