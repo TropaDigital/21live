b16faf86bd9a832d70afe5ba56c1e67697f8382a
Fino59 <slipdan@hotmail.com>
2023-04-26 18:06:17 -0300

    Update Input Default component

--- src/components/Inputs/InputDefault/index.tsx
+++ src/components/Inputs/InputDefault/index.tsx
@@ -1,14 +1,9 @@
-import React, {
-  useRef,
-  useState,
-  useCallback,
-  InputHTMLAttributes,
-} from 'react';
+import React, { useRef, useState, useCallback, InputHTMLAttributes } from 'react';
 import { IconBaseProps } from 'react-icons';
 import { IoMdHelpCircle } from 'react-icons/io';
 
-import { Container, ContainerInput, Error, Alert, ErrorInputMessage } from './styles';
 import { dateTimeMask } from './masks';
+import { Container, ContainerInput, Error, Alert, ErrorInputMessage } from './styles';
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   label?: string;
@@ -19,7 +14,15 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   isLoading?: boolean;
 }
 
-export function InputDefault({ isLoading, mask, label, alert, error, icon: Icon, ...rest }: InputProps) {
+export function InputDefault({
+  isLoading,
+  mask,
+  label,
+  alert,
+  error,
+  icon: Icon,
+  ...rest
+}: InputProps) {
   const inputRef = useRef<HTMLInputElement>(null);
 
   const [isFocused, setIsFocused] = useState(false);
@@ -35,22 +38,23 @@ export function InputDefault({ isLoading, mask, label, alert, error, icon: Icon,
     setIsFilled(!!inputRef.current?.value);
   }, []);
 
-  const handleKeyUp = useCallback((e: React.FormEvent<HTMLInputElement>) => {
-    if (mask === 'dateTime') {
-      dateTimeMask(e)
-    }
-    return;
-  }, [mask])
+  const handleKeyUp = useCallback(
+    (e: React.FormEvent<HTMLInputElement>) => {
+      if (mask === 'dateTime') {
+        dateTimeMask(e);
+      }
+      return;
+    },
+    [mask]
+  );
 
   return (
     <Container>
-      <div className="containerAlert" style={{ display: 'flex', alignItems: 'center' }} >
-        {label && (
-          <label htmlFor={label}>{label}</label>
-        )}
+      <div className="containerAlert" style={{ display: 'flex', alignItems: 'center' }}>
+        {label && <label htmlFor={label}>{label}</label>}
         {alert && (
           <Alert title={alert ?? 'Campo obrigatório'}>
-            <IoMdHelpCircle size={18} color='#CED4DA' />
+            <IoMdHelpCircle size={18} color="#CED4DA" />
           </Alert>
         )}
       </div>
@@ -62,12 +66,11 @@ export function InputDefault({ isLoading, mask, label, alert, error, icon: Icon,
         isIcon={!!Icon}
         isLoad={isLoading}
       >
-
         <div className="leftInputElement">
           {isLoading ? (
             <div className="custom-loader" />
-            ) : (
-            Icon && <Icon color='rgba(204, 204, 204, 1)' size={22}/>
+          ) : (
+            Icon && <Icon color="rgba(204, 204, 204, 1)" size={22} />
           )}
         </div>
 
@@ -76,30 +79,28 @@ export function InputDefault({ isLoading, mask, label, alert, error, icon: Icon,
           onBlur={handleInputBlur}
           ref={inputRef}
           id={label}
-          role="presentation" autoComplete="off"
+          role="presentation"
+          autoComplete="off"
           onKeyUp={handleKeyUp}
           // required={alert ? true : false}
           {...rest}
         />
-
-    
-
         {/* {error && (
           <Error title={error}>
             <FiAlertCircle size={20} color="#E62965" />
           </Error>
         )} */}
 
-          {/* <div className="errorMessage">
+        {/* <div className="errorMessage">
             <span>{error.message}</span>
           </div> */}
       </ContainerInput>
 
       {error && (
-          <ErrorInputMessage title={error}>
-            <span>{error}</span>
-          </ErrorInputMessage>
-        )}
+        <ErrorInputMessage title={error}>
+          <span>{error}</span>
+        </ErrorInputMessage>
+      )}
     </Container>
   );
 }
--- src/components/Inputs/InputDefault/styles.ts
+++ src/components/Inputs/InputDefault/styles.ts
@@ -25,6 +25,7 @@ export const Container = styled.div`
   display: flex;
   flex-direction: column;
   position: relative;
+  flex: 1;
 
   label {
     font-size: var(--text-small-sm);
