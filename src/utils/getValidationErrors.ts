b70dd34d02093b691b6eb99487e04f57dff3e45e
Fino59 <slipdan@hotmail.com>
2023-04-26 18:10:50 -0300

    Update Get Validation Erros utils

--- src/utils/getValidationErrors.ts
+++ src/utils/getValidationErrors.ts
@@ -1,15 +1,15 @@
-
+/* eslint-disable @typescript-eslint/no-unused-vars */
 interface Errors {
   [key: string]: string;
 }
 
-export default function getVaidationErrors(err: any): Errors {
+export default function getValidationErrors(err: any): Errors {
   const validationErrors: Errors = {};
 
   err.map((row: any, key: any) => {
-    const str = row.name.replace(/\s/g, '')
-    validationErrors[str] = row.error
-  })
+    const str = row.name.replace(/\s/g, '');
+    validationErrors[str] = row.error;
+  });
 
   // err.inner.forEach((error: any) => {
   //   validationErrors[error.path] = error.message;
