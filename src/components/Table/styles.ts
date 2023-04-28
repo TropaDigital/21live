8a26a8f91b122770d59c1cdb18faecb4a354bccd
Fino59 <slipdan@hotmail.com>
2023-04-28 18:21:46 -0300

    Update Table component style to width 100%

--- src/components/Table/styles.ts
+++ src/components/Table/styles.ts
@@ -129,6 +129,7 @@ export const FieldTogleButton = styled.div`
 `;
 
 export const ContainerTable = styled.div`
+  width: 100%;
   display: grid;
   grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
   gap: 1rem;
