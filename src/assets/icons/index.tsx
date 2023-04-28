2e74606b7c9c87b75cbb3396c75c896681e918db
Fino59 <slipdan@hotmail.com>
2023-04-28 18:19:16 -0300

    Update Icons assets

--- src/assets/icons/index.tsx
+++ src/assets/icons/index.tsx
@@ -296,6 +296,18 @@ export const IconArrowLeft: React.FC = () => (
   </svg>
 );
 
+export const IconArrowDown: React.FC = () => (
+  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M8 10L12 14L16 10"
+      stroke="black"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+
 export const IconTrash: React.FC = () => (
   <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path
