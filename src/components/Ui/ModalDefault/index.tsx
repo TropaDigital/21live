8367df0658a45e7e8050d701449d436135ae71ad
Fino59 <slipdan@hotmail.com>
2023-05-04 18:12:53 -0300

    Update Modal Default component to pass props width and button close

--- src/components/Ui/ModalDefault/index.tsx
+++ src/components/Ui/ModalDefault/index.tsx
@@ -3,25 +3,36 @@ import { BiX } from 'react-icons/bi';
 import * as Dialog from '@radix-ui/react-dialog';
 
 interface Props {
-  title: string;
+  title?: string;
   isOpen: boolean;
   onOpenChange?(open: boolean): void;
   children: JSX.Element;
+  closeBtn?: boolean;
+  maxWidth?: string;
 }
 
-export default function ModalDefault({ children, onOpenChange, isOpen, title }: Props) {
+export default function ModalDefault({
+  children,
+  onOpenChange,
+  isOpen,
+  title,
+  closeBtn,
+  maxWidth
+}: Props) {
   return (
     <Dialog.Root open={isOpen} onOpenChange={onOpenChange}>
       <Dialog.Portal>
         <Dialog.Overlay className="DialogOverlay" />
-        <Dialog.Content className="DialogContent">
-          <Dialog.Title className="DialogTitle">{title}</Dialog.Title>
+        <Dialog.Content className="DialogContent" style={{ maxWidth: maxWidth }}>
+          {title && <Dialog.Title className="DialogTitle">{title}</Dialog.Title>}
           {children}
-          <Dialog.Close asChild>
-            <button className="IconButton" aria-label="Close">
-              <BiX size={30} color="#6C757D" />
-            </button>
-          </Dialog.Close>
+          {closeBtn && (
+            <Dialog.Close asChild>
+              <button className="IconButton" aria-label="Close">
+                <BiX size={30} color="#6C757D" />
+              </button>
+            </Dialog.Close>
+          )}
         </Dialog.Content>
       </Dialog.Portal>
     </Dialog.Root>
