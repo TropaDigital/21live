b4b31cd8a37464f059b8a2156df9dc6b0102b8df
Fino59 <slipdan@hotmail.com>
2023-05-16 18:47:35 -0300

    Update Types with Tasks types

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,4 @@
+// Types for Product
 export interface IProduct {
   project_id?: number;
   service: string;
@@ -48,4 +49,65 @@ export interface IServices {
   service_id: string;
   size: string;
   type: string;
+}
+
+// Types for Tasks
+export interface ITaskCreate {
+  task_id?: number;
+  title: string;
+  tenant_id: number | string;
+  product_id: number | string; // produto principal, o produto pode ter uma flag que significa que ele lista outros produtos na criação da task
+  type?: string; //type_id da tabela task_type
+  flow_id?: number;
+  description: string; //descricao geral
+  creation?: ICreation; //entrega de criação
+  copywriting?: ICreation; //entrega da redação
+  products_task?: Array<IProductTask>; //esse só abre se o produto tiver a flag, irá abrir uma lista de produtos, que entrará nesta array(caso a entrega for única)
+  archives?: Array<IDocTask>;
+  deadlines?: Array<IDelivery>; //se for dividir a entrega, entra agqui
+  step?: number;
+}
+
+export interface IDelivery {
+  task_id?: number;
+  date_end: Date;
+  description?: string;
+  products?: Array<IProductTask>;
+}
+
+export interface IDocTask {
+  file_id?: number;
+  task_id?: number;
+  file_name: string;
+  size: number;
+  url: string;
+  key: string;
+  bucket: string;
+}
+
+export interface IProductTask {
+  product_task_id?: number;
+  task_id: number;
+  product_id: number;
+  service: string;
+  description: string;
+  reason_change: string; //CRIAÇÃO, DESMEMBRAMENTO, ALTERAÇÃO: INTERNA, EXTERNA.
+  type: string; //impresso ou digital
+  size: string;
+  flag?: string;
+  minutes: number | string;
+  quantity: number;
+  period: string; // monthly yearly
+}
+
+export interface ICreation {
+  description?: string;
+  date_end?: string;
+}
+
+export interface ICommentTask {
+  comment_id?: number;
+  user_id: number;
+  task_id: number;
+  comment: string;
 }
