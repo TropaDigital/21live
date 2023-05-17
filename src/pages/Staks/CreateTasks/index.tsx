4fa0a850fb0e421e19cc0b45b3084679ed252efa
Fino59 <slipdan@hotmail.com>
2023-05-17 18:48:26 -0300

    Update Create Tasks page

--- src/pages/Staks/CreateTasks/index.tsx
+++ src/pages/Staks/CreateTasks/index.tsx
@@ -1,36 +1,56 @@
 /* eslint-disable import-helpers/order-imports */
 // React
 import { useState } from 'react';
-
-// Hooks
-import useForm from '../../../hooks/useForm';
+import { Link } from 'react-router-dom';
 
 // components
 import HeaderStepsPage from '../../../components/HeaderStepsPage';
 import InfoGeral from '../ComponentSteps/InfoGeral';
+import InfoDescription from '../ComponentSteps/InfoDescription';
+import ButtonDefault from '../../../components/Buttons/ButtonDefault';
+import ModalDefault from '../../../components/Ui/ModalDefault';
+import QuantityCounter from '../../../components/QuantityCounter';
+import { CheckboxDefault } from '../../../components/Inputs/CheckboxDefault';
+import { SwitchSelector } from '../../../components/CardProductsSelected/styles';
+import InputSwitchDefault from '../../../components/Inputs/InputSwitchDefault';
+import { InputDefault } from '../../../components/Inputs/InputDefault';
 
 // Styles
 import {
+  AddProductButton,
   CloseModalButton,
   ContainerWrapper,
+  Deliveries,
   Footer,
   FormTitle,
   FormWrapper,
+  Product,
+  ProductListHeader,
   ProductListWrapper,
   ProductModalTitle,
   ProductsModalTop,
   ProductsModalWrapper,
-  SearchProductsModal
+  ProductsTable,
+  SearchProductsModal,
+  SplitDeliveries
 } from './styles';
-import InfoDescription from '../ComponentSteps/InfoDescription';
-import ButtonDefault from '../../../components/Buttons/ButtonDefault';
-import { Link } from 'react-router-dom';
+
+// Hooks
 import { useToast } from '../../../hooks/toast';
-import { ITaskCreate } from '../../../types';
 import { useFetch } from '../../../hooks/useFetch';
+import useDebouncedCallback from '../../../hooks/useDebounced';
+
+// Types
+import { IProduct, ServicesProps } from '../../../types';
+
+// Utils
 import { TenantProps } from '../../../utils/models';
-import ModalDefault from '../../../components/Ui/ModalDefault';
+
+// Icons
 import { IconClose } from '../../../assets/icons';
+import { BiCalendar, BiPencil, BiSearchAlt } from 'react-icons/bi';
+import { FiMenu } from 'react-icons/fi';
+import { SelectDefault } from '../../../components/Inputs/SelectDefault';
 
 interface StateProps {
   [key: string]: any;
@@ -44,7 +64,7 @@ type HandleOnChange = (
 ) => void;
 
 export default function CreateTasks() {
-  const [createStep, setCreateStep] = useState<number>(1);
+  const [createStep, setCreateStep] = useState<number>(2);
   const { addToast } = useToast();
 
   const { data: dataClient } = useFetch<TenantProps[]>('tenant');
@@ -57,12 +77,39 @@ export default function CreateTasks() {
     flow: '',
     description: ''
   });
-  const [productsModal, setProductsModal] = useState<boolean>(true);
+  const [search, setSearch] = useState('');
+  const [searchTerm, setSearchTerm] = useState('');
+  const [productsModal, setProductsModal] = useState<boolean>(false);
+  const [deliveriesSplit, setDeliveriesSplit] = useState<string>('no-split');
+  const {
+    data: dataProducts,
+    pages,
+    fetchData
+  } = useFetch<ServicesProps[]>(`services?search=${search}`);
+  const { isLoading, debouncedCallback } = useDebouncedCallback(
+    (search: string) => setSearch(search),
+    700
+  );
+  const [deliveryDates, setDeliveryDates] = useState({
+    dateStart: '',
+    creationDate: ''
+  });
+  const splitDeliveries = deliveriesSplit === 'mensal' ? false : true;
+  const [formatType, setFormatType] = useState<string>('');
+  const [descriptionText, setDescriptionText] = useState<string>('');
+  const handleSwitch = (value: any) => {
+    setDeliveriesSplit(value === true ? 'split' : 'no-split');
+  };
 
   const handleDescription = (value: any) => {
     // setDTOForm((prevState: any) => ({ ...prevState, ['description']: value }));
     console.log('Log do description', value);
     setDTOForm((prevState: any) => ({ ...prevState, ['description']: value }));
+  };
+
+  const handleOnChangeCheckbox = (value: any, product: IProduct) => {
+    console.log('log do value checkbox', value);
+    console.log('log do product checkbox', product);
   };
 
   function setErrorInput(value: any, message: any) {
@@ -161,6 +208,7 @@ export default function CreateTasks() {
               <FormTitle>Geral</FormTitle>
               <InfoGeral
                 data={DTOForm}
+                dataProducts={dataProducts}
                 handleInputChange={handleChangeInput}
                 clients={dataClient}
                 error={error}
@@ -180,10 +228,142 @@ export default function CreateTasks() {
             </>
           )}
           {createStep === 2 && (
-            <div>
-              teste
-              <div>sei lá</div>
-            </div>
+            <>
+              <FormTitle>Entregáveis</FormTitle>
+              <SplitDeliveries>
+                <SwitchSelector>
+                  <InputSwitchDefault
+                    onChange={(e) => {
+                      handleSwitch(e.target.checked);
+                    }}
+                    value={String(splitDeliveries)}
+                  />
+                  <span>Dividir entregas</span>
+                </SwitchSelector>
+                <Deliveries>
+                  <InputDefault
+                    label="Entrega {Nome do input intermediário}"
+                    placeholder="00/00/0000"
+                    name="dateStart"
+                    type="date"
+                    icon={BiCalendar}
+                    onChange={(e) =>
+                      setDeliveryDates({ ...deliveryDates, ['dateStart']: e.target.value })
+                    }
+                    value={deliveryDates.dateStart}
+                  />
+
+                  <InputDefault
+                    label="Entrega Criação"
+                    placeholder="00/00/0000"
+                    name="creationDate"
+                    type="date"
+                    icon={BiCalendar}
+                    onChange={(e) =>
+                      setDeliveryDates({ ...deliveryDates, ['creationDate']: e.target.value })
+                    }
+                    value={deliveryDates.creationDate}
+                  />
+                </Deliveries>
+              </SplitDeliveries>
+              <ProductsTable>
+                <FormTitle>Produtos</FormTitle>
+                <table>
+                  <thead>
+                    <tr>
+                      <th>#</th>
+                      <th>Produto</th>
+                      <th>Descrição</th>
+                      <th>Formato</th>
+                      <th>Tipo</th>
+                      <th>I/D</th>
+                      <th style={{ display: 'grid', placeItems: 'center' }}></th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {[0, 1, 2].map((row: any, index) => (
+                      <tr key={index}>
+                        <td>#{index + 1}</td>
+                        <td style={{ minWidth: '150px' }}>Produto {index + 1}</td>
+                        <td>
+                          <InputDefault
+                            label=""
+                            name="description"
+                            placeholder="Lorem ipsum dolor sit malesuada"
+                            value={descriptionText}
+                            maxLength={40}
+                            type={'text'}
+                            onChange={(e: any) => setDescriptionText(e.target.value.slice(0, 40))}
+                            error={error?.date_start}
+                          />
+                        </td>
+                        <td
+                          style={{
+                            display: 'flex',
+                            alignItems: 'center',
+                            gap: '18px',
+                            height: '82px'
+                          }}
+                        >
+                          <InputDefault
+                            label=""
+                            name="format"
+                            placeholder="128x190"
+                            value={formatType}
+                            onChange={(e: any) => setFormatType(e.target.value)}
+                            error={error?.date_start}
+                          />
+                          <div style={{ cursor: 'pointer' }}>
+                            <BiPencil />
+                          </div>
+                        </td>
+                        <td style={{ minWidth: '220px' }}>
+                          <SelectDefault
+                            label=""
+                            name="type"
+                            value={''}
+                            onChange={(e: any) => console.log('log do select type', e)}
+                            placeHolder="Selecione..."
+                          >
+                            <option value="external_change">Alteração Externa</option>
+                            <option value="creation">Criação</option>
+                          </SelectDefault>
+                        </td>
+                        <td style={{ minWidth: '220px' }}>
+                          <SelectDefault
+                            label=""
+                            name="I/D"
+                            value={''}
+                            onChange={(e: any) => console.log('log do select I/D', e)}
+                            placeHolder="Selecione..."
+                          >
+                            <option value="impressao">Impressão</option>
+                            <option value="digital">Digital</option>
+                          </SelectDefault>
+                        </td>
+                        <td style={{ cursor: 'pointer' }}>
+                          <FiMenu />
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+
+                  {/* <tfoot>
+                    <tr>
+                      <td colSpan={100}>
+                        <Pagination
+                          total={pages.total}
+                          perPage={pages.perPage}
+                          currentPage={selected}
+                          lastPage={pages.lastPage}
+                          onClickPage={(e) => setSelected(e)}
+                        />
+                      </td>
+                    </tr>
+                  </tfoot> */}
+                </table>
+              </ProductsTable>
+            </>
           )}
         </FormWrapper>
 
@@ -230,8 +410,59 @@ export default function CreateTasks() {
             </ProductsModalTop>
 
             <ProductListWrapper>
-              <SearchProductsModal></SearchProductsModal>
+              <SearchProductsModal>
+                <InputDefault
+                  label=""
+                  name="search"
+                  placeholder="Buscar produtos"
+                  onChange={(event) => {
+                    setSearchTerm(event.target.value);
+                    debouncedCallback(event.target.value);
+                  }}
+                  value={searchTerm}
+                  icon={BiSearchAlt}
+                  isLoading={isLoading}
+                  className="search-field"
+                />
+              </SearchProductsModal>
+              <ProductListHeader>
+                <div className="list-title">Produto</div>
+                <div className="list-title">Categoria</div>
+                <div className="list-title center">Quantidade</div>
+              </ProductListHeader>
+
+              {dataProducts?.map((row: any, index) => (
+                <Product key={index}>
+                  <div className="product">
+                    <CheckboxDefault
+                      label=""
+                      name={row.service_id}
+                      onChange={(e: any) => handleOnChangeCheckbox(e, row)}
+                      // checked={data.email_alert === 'true' ? true : false}
+                      checked={false}
+                    />
+                    {row.service}
+                  </div>
+                  <div className="category">{row.category}</div>
+                  <div className="quantity">
+                    <QuantityCounter />
+                  </div>
+                </Product>
+              ))}
             </ProductListWrapper>
+
+            <AddProductButton>
+              <ButtonDefault
+                typeButton="primary"
+                onClick={() => {
+                  console.log('add product');
+                  setProductsModal(false);
+                  setCreateStep(createStep + 1);
+                }}
+              >
+                Adicionar Produto
+              </ButtonDefault>
+            </AddProductButton>
           </ProductsModalWrapper>
         </ModalDefault>
       </ContainerWrapper>
--- src/pages/Staks/CreateTasks/styles.ts
+++ src/pages/Staks/CreateTasks/styles.ts
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
 
-export const ContainerWrapper = styled.div``;
+export const ContainerWrapper = styled.div`
+  padding-bottom: 100px;
+`;
 
 export const FormWrapper = styled.div`
   padding: 40px 30px;
@@ -68,42 +70,192 @@ export const Footer = styled.div`
 `;
 
 export const ProductsModalWrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    gap: 24px;
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
 `;
 
 export const ProductsModalTop = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 `;
 
 export const ProductModalTitle = styled.h5`
-    color: var(--gray-900);
-    font-size: var(--text-small-xl);
-    font-weight: var(--weight-semibold);
-    line-height: 30px;
+  color: var(--gray-900);
+  font-size: var(--text-small-xl);
+  font-weight: var(--weight-semibold);
+  line-height: 30px;
 `;
 
 export const CloseModalButton = styled.button`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    width: 32px;
-    height: 32px;
-    background-color: transparent;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 32px;
+  height: 32px;
+  background-color: transparent;
 `;
 
 export const ProductListWrapper = styled.div`
-    height: fit-content;
-    border: 1px solid var(--gray-100);
-    border-radius: 8px;
+  height: fit-content;
+  border: 1px solid var(--gray-100);
+  border-radius: 8px;
 `;
 
 export const SearchProductsModal = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 68px;
+  padding: 16px 12px;
+`;
+
+export const ProductListHeader = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 136px;
+  height: 44px;
+  background: var(--gray-50);
+  border-bottom: 1px solid var(--gray-200);
+  padding: 13px 0;
+
+  .list-title {
+    color: var(--gray-500);
+    font-size: var(--text-small-xs);
+    font-weight: var(--weight-medium);
+    padding-left: 24px;
+
+    &.center {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      padding-left: 0;
+    }
+  }
+`;
+
+export const Product = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 136px;
+  height: 72px;
+  border-bottom: 1px solid var(--gray-200);
+  padding: 26px 0;
+
+  .product {
+    display: flex;
+    align-items: center;
+    gap: 8px;
+
+    color: var(--gray-700);
+    font-size: var(--text-small-sm);
+    font-weight: var(--weight-medium);
+    text-transform: uppercase;
+    padding-left: 24px;
+  }
+
+  .category {
+    color: var(--gray-700);
+    font-size: var(--text-small-sm);
+    font-weight: var(--weight-medium);
+    padding-left: 24px;
+
+    text-transform: uppercase;
+  }
+
+  .quantity {
     display: flex;
     align-items: center;
     justify-content: center;
-    height: 68px;
+  }
+`;
+
+export const AddProductButton = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  height: 44px;
+`;
+
+export const SplitDeliveries = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
+
+export const Deliveries = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 16px;
+  height: 82px;
+  max-width: 656px;
+`;
+
+export const ProductsTable = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  margin-top: 38px;
+
+  table {
+    width: 100%;
+    border-spacing: 0;
+
+    /* tbody > tr:nth-child(2n) td {
+      background-color: var(--gray-50);
+    } */
+
+    /* tr {
+      background-color: var(--gray-50);
+      width: 100%;
+      border: 0;
+    } */
+
+    tfoot {
+      td {
+        padding: 0 1rem;
+        border-bottom: none;
+      }
+    }
+
+    th {
+      background-color: var(--gray-50);
+      border-bottom: 1px solid #eaecf0;
+      /* border-radius: 5px; */
+      text-transform: capitalize;
+      font-size: var(--text-small-xs);
+      font-weight: var(--weight-medium);
+      color: var(--gray-500);
+      padding: 0.75rem 1.5rem;
+      text-align: left;
+      line-height: 20px;
+    }
+
+    td {
+      font-size: var(--text-small-sm);
+      font-weight: var(--weight-medium);
+      color: var(--gray-600);
+      padding: 1rem 1.5rem;
+      background: #ffffff;
+      /* border-radius: 5px; */
+      border-bottom: 1px solid #eaecf0;
+      text-align: left;
+
+      white-space: nowrap;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      max-width: 38ch;
+
+      .fieldTableClients {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        gap: 5px;
+      }
+    }
+    .container {
+      margin: 100px auto;
+      width: 500px;
+      text-align: center;
+    }
+  }
 `;
