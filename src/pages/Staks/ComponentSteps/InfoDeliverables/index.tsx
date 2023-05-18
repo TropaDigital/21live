550e797240967d9c328f1f85e95f2e05e785e0b2
Fino59 <slipdan@hotmail.com>
2023-05-18 18:31:12 -0300

    Create Info Deliverables page

---
+++ src/pages/Staks/ComponentSteps/InfoDeliverables/index.tsx
@@ -0,0 +1,368 @@
+/* eslint-disable import-helpers/order-imports */
+// React
+import { useState } from 'react';
+
+// Icons
+import { BiPencil } from 'react-icons/bi';
+import { FiChevronDown, FiChevronUp, FiMenu } from 'react-icons/fi';
+
+// Components
+import { InputDefault } from '../../../../components/Inputs/InputDefault';
+import { SelectDefault } from '../../../../components/Inputs/SelectDefault';
+
+// Styles
+import { FormTitle } from '../../../CreateProject/styles';
+import {
+  AddNewDelivery,
+  Deliveries,
+  DeliveryTitle,
+  EditableFormat,
+  NewDelivery,
+  ProductsTable,
+  TableDelivery
+} from './styles';
+import { IconCalendar, IconPlus } from '../../../../assets/icons';
+import AddTextButton from '../../../../components/Buttons/AddTextButton';
+
+interface FormProps {
+  [key: string]: any;
+}
+
+interface TypeProps {
+  type_id: number | string;
+  name: string;
+  percent: number | string;
+}
+
+interface Props {
+  data: any;
+  dataTypes: any;
+  handleProducts: () => void;
+  error: FormProps;
+  deliveriesSplited: boolean;
+}
+
+interface OpenMenuProps {
+  deliveryId: number | string;
+  openInfo: boolean;
+}
+
+export default function InfoDeliveries({
+  data,
+  dataTypes,
+  handleProducts,
+  error,
+  deliveriesSplited
+}: Props) {
+  const [descriptionText, setDescriptionText] = useState<any>({
+    inputId: '',
+    text: ''
+  });
+  const [formatType, setFormatType] = useState<any>('');
+  const [editFormat, setEditFormat] = useState<any>({
+    productIndex: '',
+    editable: true
+  });
+  const [showDeliveryInfos, setShowDeliveryInfos] = useState<OpenMenuProps>({
+    deliveryId: '',
+    openInfo: false
+  });
+
+  return (
+    <>
+      {!deliveriesSplited && (
+        <ProductsTable>
+          <FormTitle>Produtos</FormTitle>
+          <table>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Produto</th>
+                <th>Descrição</th>
+                <th>Formato</th>
+                <th>Tipo</th>
+                <th>I/D</th>
+                <th style={{ display: 'grid', placeItems: 'center' }}></th>
+              </tr>
+            </thead>
+            <tbody>
+              {[0, 1, 2].map((row: any, index) => (
+                <tr key={index}>
+                  <td>#{index + 1}</td>
+                  <td style={{ minWidth: '150px' }}>Produto {index + 1} com texto gigante </td>
+                  <td>
+                    <div
+                      style={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        gap: '12px',
+                        minWidth: '100%'
+                      }}
+                    >
+                      <InputDefault
+                        label=""
+                        name="description"
+                        placeholder="Lorem ipsum dolor sit malesuada"
+                        value={descriptionText.inputId === index ? descriptionText.text : ''}
+                        maxLength={40}
+                        type={'text'}
+                        disabled={descriptionText.inputId !== index}
+                        onChange={(e: any) =>
+                          setDescriptionText({ inputId: index, text: e.target.value.slice(0, 40) })
+                        }
+                        //   error={error?.date_start}
+                      />
+                      <EditableFormat
+                        className={descriptionText.inputId === index ? 'edit' : ''}
+                        onClick={() => {
+                          setDescriptionText({ inputId: index, text: '' });
+                        }}
+                      >
+                        <BiPencil />
+                      </EditableFormat>
+                    </div>
+                  </td>
+                  <td>
+                    <div
+                      style={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        gap: '18px',
+                        height: '82px'
+                      }}
+                    >
+                      <InputDefault
+                        label=""
+                        name="format"
+                        placeholder="128x190"
+                        value={editFormat.productIndex === index ? formatType : ''}
+                        disabled={
+                          editFormat.productIndex === index && editFormat.editable ? false : true
+                        }
+                        onChange={(e: any) => setFormatType(e.target.value)}
+                        //   error={error?.date_start}
+                      />
+                      <EditableFormat
+                        className={editFormat.productIndex === index ? 'edit' : ''}
+                        onClick={() => {
+                          setEditFormat({
+                            productIndex: index,
+                            editable: true
+                          });
+                          setFormatType('');
+                        }}
+                      >
+                        <BiPencil />
+                      </EditableFormat>
+                    </div>
+                  </td>
+                  <td style={{ minWidth: '220px' }}>
+                    <SelectDefault
+                      label=""
+                      name="type"
+                      value={''}
+                      onChange={(e: any) => console.log('log do select type', e)}
+                      placeHolder="Selecione..."
+                    >
+                      {dataTypes?.map((row: TypeProps) => (
+                        <option key={row.type_id} value={row.type_id}>
+                          {row.name}
+                        </option>
+                      ))}
+                    </SelectDefault>
+                  </td>
+                  <td style={{ minWidth: '220px' }}>
+                    <SelectDefault
+                      label=""
+                      name="I/D"
+                      value={''}
+                      onChange={(e: any) => console.log('log do select I/D', e)}
+                      placeHolder="Selecione..."
+                    >
+                      <option value="impressao">Impressão</option>
+                      <option value="digital">Digital</option>
+                    </SelectDefault>
+                  </td>
+                  <td style={{ cursor: 'pointer' }}>
+                    <FiMenu />
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </ProductsTable>
+      )}
+
+      {deliveriesSplited && (
+        <>
+          <Deliveries openInfos={showDeliveryInfos.openInfo}>
+            <DeliveryTitle>
+              <div className="title-delivery">
+                Primeira entrega
+                <span>-</span>
+                <div className="date">
+                  <IconCalendar /> 13/03/2023
+                </div>
+              </div>
+              <div
+                className="icon-arrow"
+                onClick={() =>
+                  setShowDeliveryInfos({
+                    deliveryId: 1,
+                    openInfo: showDeliveryInfos.openInfo ? false : true
+                  })
+                }
+              >
+                {showDeliveryInfos.deliveryId === 1 && showDeliveryInfos.openInfo ? (
+                  <FiChevronUp />
+                ) : (
+                  <FiChevronDown />
+                )}
+              </div>
+            </DeliveryTitle>
+            <div style={{ padding: '24px' }}>
+              <TableDelivery>
+                <table>
+                  <thead>
+                    <tr>
+                      <th>#</th>
+                      <th>Produto</th>
+                      <th>Descrição</th>
+                      <th>Formato</th>
+                      <th>Tipo</th>
+                      <th>I/D</th>
+                      <th style={{ display: 'grid', placeItems: 'center', height: '45px' }}></th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {[0].map((row: any, index) => (
+                      <tr key={index}>
+                        <td>#{index + 1}</td>
+                        <td style={{ minWidth: '150px' }}>
+                          Produto {index + 1} com texto gigante{' '}
+                        </td>
+                        <td>
+                          <div
+                            style={{
+                              display: 'flex',
+                              alignItems: 'center',
+                              gap: '12px',
+                              minWidth: '100%'
+                            }}
+                          >
+                            <InputDefault
+                              label=""
+                              name="description"
+                              placeholder="Lorem ipsum dolor sit malesuada"
+                              value={descriptionText.inputId === index ? descriptionText.text : ''}
+                              maxLength={40}
+                              type={'text'}
+                              disabled={descriptionText.inputId !== index}
+                              onChange={(e: any) =>
+                                setDescriptionText({
+                                  inputId: index,
+                                  text: e.target.value.slice(0, 40)
+                                })
+                              }
+                              //   error={error?.date_start}
+                            />
+                            <EditableFormat
+                              className={descriptionText.inputId === index ? 'edit' : ''}
+                              onClick={() => {
+                                setDescriptionText({ inputId: index, text: '' });
+                              }}
+                            >
+                              <BiPencil />
+                            </EditableFormat>
+                          </div>
+                        </td>
+                        <td>
+                          <div
+                            style={{
+                              display: 'flex',
+                              alignItems: 'center',
+                              gap: '18px',
+                              height: '82px'
+                            }}
+                          >
+                            <InputDefault
+                              label=""
+                              name="format"
+                              placeholder="128x190"
+                              value={editFormat.productIndex === index ? formatType : ''}
+                              disabled={
+                                editFormat.productIndex === index && editFormat.editable
+                                  ? false
+                                  : true
+                              }
+                              onChange={(e: any) => setFormatType(e.target.value)}
+                              //   error={error?.date_start}
+                            />
+                            <EditableFormat
+                              className={editFormat.productIndex === index ? 'edit' : ''}
+                              onClick={() => {
+                                setEditFormat({
+                                  productIndex: index,
+                                  editable: true
+                                });
+                                setFormatType('');
+                              }}
+                            >
+                              <BiPencil />
+                            </EditableFormat>
+                          </div>
+                        </td>
+                        <td style={{ minWidth: '220px' }}>
+                          <SelectDefault
+                            label=""
+                            name="type"
+                            value={''}
+                            onChange={(e: any) => console.log('log do select type', e)}
+                            placeHolder="Selecione..."
+                          >
+                            {dataTypes?.map((row: TypeProps) => (
+                              <option key={row.type_id} value={row.type_id}>
+                                {row.name}
+                              </option>
+                            ))}
+                          </SelectDefault>
+                        </td>
+                        <td style={{ minWidth: '220px' }}>
+                          <SelectDefault
+                            label=""
+                            name="I/D"
+                            value={''}
+                            onChange={(e: any) => console.log('log do select I/D', e)}
+                            placeHolder="Selecione..."
+                          >
+                            <option value="impressao">Impressão</option>
+                            <option value="digital">Digital</option>
+                          </SelectDefault>
+                        </td>
+                        <td style={{ cursor: 'pointer' }}>
+                          <FiMenu />
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </TableDelivery>
+              <AddTextButton
+                title="Adicionar produto"
+                click={() => console.log('log do add products')}
+              />
+            </div>
+          </Deliveries>
+          <NewDelivery>
+            <AddNewDelivery>
+              <div className="plus">
+                <IconPlus />
+              </div>
+              Adicionar nova entrega
+            </AddNewDelivery>
+          </NewDelivery>
+        </>
+      )}
+    </>
+  );
+}
--- src/pages/Staks/ComponentSteps/InfoDeliverables/styles.ts
+++ src/pages/Staks/ComponentSteps/InfoDeliverables/styles.ts
@@ -1,125 +1,8 @@
 import styled from 'styled-components';
 
-export const ProductsModalWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 24px;
-`;
-
-export const ProductsModalTop = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-`;
-
-export const ProductModalTitle = styled.h5`
-  color: var(--gray-900);
-  font-size: var(--text-small-xl);
-  font-weight: var(--weight-semibold);
-  line-height: 30px;
-`;
-
-export const CloseModalButton = styled.button`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 32px;
-  height: 32px;
-  background-color: transparent;
-`;
-
-export const ProductListWrapper = styled.div`
-  height: fit-content;
-  border: 1px solid var(--gray-100);
-  border-radius: 8px;
-`;
-
-export const SearchProductsModal = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 68px;
-  padding: 16px 12px;
-`;
-
-export const ProductListHeader = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr 136px;
-  height: 44px;
-  background: var(--gray-50);
-  border-bottom: 1px solid var(--gray-200);
-  padding: 13px 0;
-
-  .list-title {
-    color: var(--gray-500);
-    font-size: var(--text-small-xs);
-    font-weight: var(--weight-medium);
-    padding-left: 24px;
-
-    &.center {
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      padding-left: 0;
-    }
-  }
-`;
-
-export const Product = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr 136px;
-  height: 72px;
-  border-bottom: 1px solid var(--gray-200);
-  padding: 26px 0;
-
-  .product {
-    display: flex;
-    align-items: center;
-    gap: 8px;
-
-    color: var(--gray-700);
-    font-size: var(--text-small-sm);
-    font-weight: var(--weight-medium);
-    text-transform: uppercase;
-    padding-left: 24px;
-  }
-
-  .category {
-    color: var(--gray-700);
-    font-size: var(--text-small-sm);
-    font-weight: var(--weight-medium);
-    padding-left: 24px;
-
-    text-transform: uppercase;
-  }
-
-  .quantity {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
-`;
-
-export const AddProductButton = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: flex-end;
-  height: 44px;
-`;
-
-export const SplitDeliveries = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 20px;
-`;
-
-export const Deliveries = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 16px;
-  height: 82px;
-  max-width: 656px;
-`;
+interface DeliveryProps {
+  openInfos: boolean;
+}
 
 export const ProductsTable = styled.div`
   display: flex;
@@ -189,4 +72,164 @@ export const ProductsTable = styled.div`
       text-align: center;
     }
   }
+`;
+
+export const EditableFormat = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 20px;
+  height: 20px;
+  cursor: pointer;
+
+  &.edit {
+    background-color: var(--primary-50);
+    border-radius: 50%;
+  }
+`;
+
+export const Deliveries = styled.div<DeliveryProps>`
+  display: flex;
+  flex-direction: column;
+  height: ${({ openInfos }) => (openInfos ? 'fit-content' : '60px')};
+  background: var(--background-primary);
+  border: 1px solid var(--gray-200);
+  border-radius: 10px;
+  margin-top: 28px;
+  overflow: hidden;
+
+  table {
+    width: 100%;
+    border-spacing: 0;
+
+    /* tbody > tr:nth-child(2n) td {
+      background-color: var(--gray-50);
+    } */
+
+    /* tr {
+      background-color: var(--gray-50);
+      width: 100%;
+      border: 0;
+    } */
+
+    tfoot {
+      td {
+        padding: 0 1rem;
+        border-bottom: none;
+      }
+    }
+
+    th {
+      background-color: var(--gray-50);
+      border-bottom: 1px solid #eaecf0;
+      /* border-radius: 5px; */
+      text-transform: capitalize;
+      font-size: var(--text-small-xs);
+      font-weight: var(--weight-medium);
+      color: var(--gray-500);
+      padding: 0.75rem 1.5rem;
+      text-align: left;
+      line-height: 20px;
+    }
+
+    td {
+      font-size: var(--text-small-sm);
+      font-weight: var(--weight-medium);
+      color: var(--gray-600);
+      padding: 1rem 1.5rem;
+      background: #ffffff;
+      /* border-radius: 5px; */
+      border-bottom: 1px solid #eaecf0;
+      text-align: left;
+
+      white-space: nowrap;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      max-width: 38ch;
+
+      .fieldTableClients {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        gap: 5px;
+      }
+    }
+    .container {
+      margin: 100px auto;
+      width: 500px;
+      text-align: center;
+    }
+  }
+`;
+
+export const DeliveryTitle = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  min-height: 60px;
+  padding: 16px 24px;
+  border-bottom: 1px solid var(--gray-200);
+
+  .title-delivery {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 12px;
+    color: var(--gray-700);
+    font-size: var(--text-small-lg);
+    font-weight: var(--weight-bold);
+
+    .date {
+      display: flex;
+      align-items: center;
+      gap: 6px;
+      color: var(--success-600);
+      font-size: var(--text-small-sm);
+      font-weight: var(--weight-medium);
+    }
+  }
+
+  .icon-arrow {
+    cursor: pointer;
+  }
+`;
+
+export const NewDelivery = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 132px;
+  background: transparent;
+  border: 2px dashed var(--gray-400);
+  border-radius: 8px;
+  padding: 24px;
+  margin-top: 16px;
+`;
+
+export const AddNewDelivery = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 8px;
+
+  color: var(--gray-600);
+  font-size: var(--text-small-lg);
+  font-weight: var(--weight-medium);
+
+  .plus {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 48px;
+    height: 48px;
+    background: var(--primary-100);
+    border-radius: 50%;
+  }
+`;
+
+export const TableDelivery = styled.div`
+  border: 1px solid var(--gray-200);
+  border-radius: 8px;
 `;
