2e190a8d7a0bcc8d4eda1cdc9de8fa09596e30bf
Fino59 <slipdan@hotmail.com>
2023-05-18 18:31:43 -0300

    Create Info Inputs page

---
+++ src/pages/Staks/ComponentSteps/InfoInputs/index.tsx
@@ -0,0 +1,43 @@
+// Components
+import WrapperEditor from '../../../../components/WrapperEditor';
+
+// Styles
+import { InputField, InputFieldTitle, InputTaskWrapper } from './styles';
+
+interface InputsProps {
+  valueFirst: string;
+  valueSecond: string;
+  handleOnDescription: (value: any) => void;
+  handleOnInput: (value: any) => void;
+  mentions: any;
+}
+
+export default function TaskInputs({
+  valueFirst,
+  valueSecond,
+  handleOnDescription,
+  handleOnInput,
+  mentions
+}: InputsProps) {
+  return (
+    <InputTaskWrapper>
+      <InputField>
+        <InputFieldTitle>Input &#123;Nome do input intermediário&#125;</InputFieldTitle>
+        <WrapperEditor
+          mentionData={mentions}
+          value={valueFirst}
+          handleOnDescription={(value: any) => handleOnDescription(value)}
+        />
+      </InputField>
+
+      <InputField>
+        <InputFieldTitle>Input criação</InputFieldTitle>
+        <WrapperEditor
+          mentionData={mentions}
+          value={valueSecond}
+          handleOnDescription={(value: any) => handleOnInput(value)}
+        />
+      </InputField>
+    </InputTaskWrapper>
+  );
+}
