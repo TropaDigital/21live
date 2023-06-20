5eeb9556fe8ff7ee4da607a5deaedf0d24156b3e
Fino59 <slipdan@hotmail.com>
2023-06-20 18:54:24 -0300

    Update Edit Fluxo page

--- src/pages/Fluxos/EditFluxo/index.tsx
+++ src/pages/Fluxos/EditFluxo/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect } from 'react';
 import { BiSave, BiShow } from 'react-icons/bi';
 import { useLocation, useNavigate } from 'react-router-dom';
