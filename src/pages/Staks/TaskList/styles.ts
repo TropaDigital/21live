644eaec20e4e12cf6f8dd5905d728bb3f99dea56
Fino59 <slipdan@hotmail.com>
2023-06-23 13:22:48 -0300

    Update Task List page

--- src/pages/Staks/TaskList/styles.ts
+++ src/pages/Staks/TaskList/styles.ts
@@ -1,7 +1,23 @@
+/* eslint-disable prettier/prettier */
 import styled from 'styled-components';
 
 export const ModalShowTaskWrapper = styled.div`
   display: flex;
   flex-direction: column;
   gap: 32px;
+`;
+
+export const TableFlag = styled.td`
+  svg {
+    width: 20px;
+    height: 20px;
+  }
+
+  &.flagged {
+    svg {
+      path {
+        fill: #F04438;
+      }
+    }
+  }
 `;
