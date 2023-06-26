3235d03b72bc475f78d81005ce0e8fcab2f8ae01
Fino59 <slipdan@hotmail.com>
2023-06-26 18:54:58 -0300

    Update Task List page

--- src/pages/Staks/TaskList/index.tsx
+++ src/pages/Staks/TaskList/index.tsx
@@ -5,6 +5,7 @@ import { Link, useNavigate } from 'react-router-dom';
 
 // Icons
 import { BiPlus, BiSearchAlt } from 'react-icons/bi';
+import { FiFlag } from 'react-icons/fi';
 import { IconContext } from 'react-icons';
 
 // Hooks
@@ -44,7 +45,6 @@ import moment from 'moment';
 
 // Styles
 import { ModalShowTaskWrapper, TableFlag } from './styles';
-import { FiFlag } from 'react-icons/fi';
 
 export default function TaskList() {
   const { addToast } = useToast();
