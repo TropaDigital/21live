0a95eb9af0f5f6563a65b6b0c9a242bf3a4d83bc
Fino59 <slipdan@hotmail.com>
2023-07-11 18:58:28 -0300

    Update Card Task Info component with types of cards

--- src/components/Ui/CardTaskInfo/index.tsx
+++ src/components/Ui/CardTaskInfo/index.tsx
@@ -1,14 +1,61 @@
-import { CardTitle, CardWrapper } from './styles';
+import { CardInfo, CardInfoField, CardTitle, CardWrapper } from './styles';
 
 interface CardTaskInfoProps {
   cardTitle: string;
   cardType: 'text' | 'time' | 'info';
+  dataCard: 'string';
 }
 
-export default function CardTaskInfo({ cardTitle, cardType }: CardTaskInfoProps) {
+export default function CardTaskInfo({ cardTitle, cardType, dataCard }: CardTaskInfoProps) {
   return (
-    <CardWrapper>
+    <CardWrapper cardSize={cardType}>
       <CardTitle>{cardTitle}</CardTitle>
+      {cardType === 'text' && (
+        <div>
+          Lorem ipsum dolor sit amet consectetur. Lectus mi urna consequat faucibus eget nunc orci.
+          Massa ornare justo erat sagittis aliquam turpis porttitor. Venenatis vestibulum malesuada
+          egestas senectus eu et ultricies dui tortor. Elementum vitae feugiat pulvinar mi sed cras.
+          Feugiat nibh nisl dignissim orci in. Imperdiet sed arcu ac consequat.
+        </div>
+      )}
+      {cardType === 'time' && (
+        <>
+          <div>timer</div>
+          <div>Tempo estimado: 00:30:00</div>
+        </>
+      )}
+      {cardType === 'info' && (
+        <CardInfo>
+          <CardInfoField>
+            <div className="info-title">Tempo estimado</div>
+            <div className="info-description">02:00:00</div>
+          </CardInfoField>
+          <CardInfoField>
+            <div className="info-title">Responsável:</div>
+            <div className="info-description">Guilherme Augusto</div>
+          </CardInfoField>
+          <CardInfoField>
+            <div className="info-title">Etapa:</div>
+            <div className="info-description">Criação</div>
+          </CardInfoField>
+          <CardInfoField>
+            <div className="info-title">Fluxo:</div>
+            <div className="info-description">Campanha</div>
+          </CardInfoField>
+          <CardInfoField>
+            <div className="info-title">Prioridade:</div>
+            <div className="info-description">Normal</div>
+          </CardInfoField>
+          <CardInfoField>
+            <div className="info-title">Data inicial:</div>
+            <div className="info-description">26 de junho</div>
+          </CardInfoField>
+          <CardInfoField>
+            <div className="info-title">Data final:</div>
+            <div className="info-description">15 de Julho</div>
+          </CardInfoField>
+        </CardInfo>
+      )}
     </CardWrapper>
   );
 }
--- src/components/Ui/CardTaskInfo/styles.ts
+++ src/components/Ui/CardTaskInfo/styles.ts
@@ -1,11 +1,15 @@
 import styled from 'styled-components';
 
-export const CardWrapper = styled.div`
+interface CardProps {
+  cardSize: string;
+}
+
+export const CardWrapper = styled.div<CardProps>`
   display: flex;
   flex-direction: column;
   gap: 16px;
 
-  /* width: 680px; */
+  width: ${({ cardSize }) => (cardSize !== 'time' ? '100%' : '264px')};
   height: 150px;
 
   background: var(--background-primary);
@@ -20,4 +24,31 @@ export const CardTitle = styled.div`
   font-size: var(--text-small-xl);
   font-weight: var(--weight-semibold);
   line-height: 30px;
+`;
+
+export const CardInfo = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+  align-self: stretch;
+
+  padding: 24px;
+`;
+
+export const CardInfoField = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+
+  font-size: var(--text-small-md);
+
+  .info-title {
+    color: var(--gray-500);
+    font-weight: var(--weight-regular);
+  }
+
+  .info-description {
+    color: var(--gray-900);
+    font-weight: var(--weight-semibold);
+  }
 `;
