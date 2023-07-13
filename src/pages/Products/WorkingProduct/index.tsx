5c3992d39f925b0530edfe9ef7d182fecc80cd36
Fino59 <slipdan@hotmail.com>
2023-07-13 18:26:06 -0300

    Update Working Product page with tabs and sections

--- src/pages/Products/WorkingProduct/index.tsx
+++ src/pages/Products/WorkingProduct/index.tsx
@@ -1,16 +1,39 @@
+/* eslint-disable import-helpers/order-imports */
+
+// React
+import { useState } from 'react';
+
 // Components
 import { useLocation } from 'react-router-dom';
 
 // Hooks
 import { useFetch } from '../../../hooks/useFetch';
 
+// Icons
+import { IconText } from '../../../assets/icons';
+import { BiInfoCircle } from 'react-icons/bi';
+import { HiOutlineArrowRight, HiOutlineChatAlt } from 'react-icons/hi';
+
 // Components
 import HeaderOpenTask from '../../../components/HeaderTaskPage';
 import CardTaskInfo from '../../../components/Ui/CardTaskInfo';
 import { ContainerDefault } from '../../../components/UiElements/styles';
+import WrapperEditor from '../../../components/WrapperEditor';
 
 // Styles
-import { CardsTopWrapper, SectionCardWrapper } from './styles';
+import {
+  CardsTopWrapper,
+  ChatSendButton,
+  InputChat,
+  InputField,
+  InputFieldTitle,
+  SectionCardWrapper,
+  SectionChatComments,
+  TabsWrapper,
+  TaskTab,
+  WorkSection
+} from './styles';
+import { InputDefault } from '../../../components/Inputs/InputDefault';
 
 interface WorkingProductProps {
   estimatedTime?: string;
@@ -28,6 +51,9 @@ interface ProjectInfo {
 
 export default function WorkingProduct() {
   const location = useLocation();
+  const [selectedTab, setSelectedTab] = useState<string>('');
+  const [notifications, setNotifications] = useState<boolean>(false);
+  const [chatMessage, setChatMessage] = useState<string>('');
 
   // const { data } = useFetch<WorkingProductProps>(`/${location.state.id}`);
 
@@ -37,6 +63,21 @@ export default function WorkingProduct() {
 
   const dataText =
     'Lorem ipsum dolor sit amet consectetur. Lectus mi urna consequat faucibus eget nunc orci. Massa ornare justo erat sagittis aliquam turpis porttitor. Venenatis vestibulum malesuada egestas senectus eu et ultricies dui tortor. Elementum vitae feugiat pulvinar mi sed cras. Feugiat nibh nisl dignissim orci in. Imperdiet sed arcu ac consequat.';
+
+  const titleInfos = {
+    idNumber: '1768',
+    numberTask: '01',
+    titleTask: 'Cronograma',
+    monthTask: 'Julho 2023',
+    client_task: 'G.WIND',
+    typeTask: 'FEE',
+    quantityTask: 'PACK 8 POSTS',
+    contract_task: 'MÊS'
+  };
+
+  const handleInputChange = (e: any) => {
+    setChatMessage(e.target.value);
+  };
 
   // const dataInfo = {
   //   estimatedTime: '02:00:00',
@@ -50,7 +91,12 @@ export default function WorkingProduct() {
 
   return (
     <ContainerDefault>
-      <HeaderOpenTask title="Teste de header" />
+      <HeaderOpenTask
+        title={titleInfos}
+        disableButton={false}
+        backPage="/tarefas"
+        buttonType="finish"
+      />
 
       <SectionCardWrapper>
         <CardsTopWrapper>
@@ -63,6 +109,85 @@ export default function WorkingProduct() {
           <CardTaskInfo cardTitle="Contexto geral" cardType="text" dataText={dataText} />
         </CardsTopWrapper>
       </SectionCardWrapper>
+
+      <TabsWrapper>
+        <TaskTab
+          onClick={(e: any) => {
+            setSelectedTab(e.target.innerText);
+          }}
+          className={selectedTab === 'Redação' ? 'active' : ''}
+        >
+          <IconText />
+          Redação
+        </TaskTab>
+        <TaskTab
+          onClick={(e: any) => {
+            setSelectedTab(e.target.innerText);
+          }}
+          className={selectedTab === 'Inputs' ? 'active' : ''}
+        >
+          <BiInfoCircle />
+          Inputs
+        </TaskTab>
+        <TaskTab
+          onClick={(e: any) => {
+            setSelectedTab(e.target.innerText);
+          }}
+          className={selectedTab === 'Comentários' ? 'active' : ''}
+        >
+          <HiOutlineChatAlt />
+          Comentários
+          {notifications && <div className="notification" />}
+        </TaskTab>
+      </TabsWrapper>
+
+      <WorkSection>
+        {selectedTab === 'Redação' && (
+          <WrapperEditor
+            value={'Texto inicial'}
+            mentionData={[]}
+            handleOnDescription={(value: any) => console.log('log do editor', value)}
+          />
+        )}
+        {selectedTab === 'Inputs' && (
+          <div style={{ display: 'flex', flexDirection: 'column', gap: '16px' }}>
+            <InputField>
+              <InputFieldTitle>Input Intermediário???</InputFieldTitle>
+              <WrapperEditor
+                value={'Teste 1'}
+                mentionData={[]}
+                handleOnDescription={(value: any) => console.log('log do editor', value)}
+              />
+            </InputField>
+
+            <InputField>
+              <InputFieldTitle>Input Criação</InputFieldTitle>
+              <WrapperEditor
+                value={'Teste 2'}
+                mentionData={[]}
+                handleOnDescription={(value: any) => console.log('log do editor', value)}
+              />
+            </InputField>
+          </div>
+        )}
+        {selectedTab === 'Comentários' && (
+          <SectionChatComments>
+            <InputChat>
+              <InputDefault
+                label=""
+                placeholder="Mensagem..."
+                name="chat"
+                value={chatMessage}
+                onChange={handleInputChange}
+                error={''}
+              />
+              <ChatSendButton>
+                <HiOutlineArrowRight />
+              </ChatSendButton>
+            </InputChat>
+          </SectionChatComments>
+        )}
+      </WorkSection>
     </ContainerDefault>
   );
 }
--- src/pages/Products/WorkingProduct/styles.ts
+++ src/pages/Products/WorkingProduct/styles.ts
@@ -13,3 +13,165 @@ export const CardsTopWrapper = styled.div`
   align-items: center;
   gap: 24px;
 `;
+
+export const TabsWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 24px;
+  height: 36px;
+
+  margin: 0 40px;
+  border-bottom: 1px solid var(--gray-200);
+`;
+
+export const TaskTab = styled.button`
+  display: flex;
+  align-items: center;
+  gap: 5px;
+
+  width: fit-content;
+  height: 100%;
+  background: transparent;
+  border-bottom: 1px solid transparent;
+
+  color: var(--gray-500);
+  font-size: var(--text-small-sm);
+  font-weight: var(--weight-semibold);
+
+  transition: all 0.3s;
+
+  svg {
+    width: 16px;
+    height: 16px;
+    path {
+      stroke: var(--gray-500);
+    }
+  }
+
+  &:hover,
+  &.active {
+    color: var(--primary-900);
+    border-radius: 0;
+    border: none;
+    border-bottom: 1px solid var(--primary-900);
+
+    svg {
+      path {
+        stroke: var(--primary-900);
+      }
+    }
+  }
+
+  .notification {
+    width: 5px;
+    height: 5px;
+    background-color: var(--error-500);
+    border-radius: 100%;
+  }
+`;
+
+export const WorkSection = styled.div`
+  margin: 24px 40px 0 40px;
+  padding-bottom: 100px;
+
+  .ProseMirror {
+    height: 260px;
+  }
+
+  @media (min-height: 800px) {
+    .ProseMirror {
+      height: 400px;
+    }
+  }
+`;
+
+export const InputField = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+
+  &.error {
+    border: 2px solid var(--input-error);
+    border-radius: 4px;
+    padding: 4px 8px;
+  }
+
+  .ProseMirror {
+    height: 160px;
+  }
+`;
+
+export const InputFieldTitle = styled.div`
+  color: var(--gray-700);
+  font-size: var(--text-small-sm);
+  font-weight: var(--weight-medium);
+
+  span {
+    color: var(--input-error);
+    font-size: var(--text-small-sm);
+    font-weight: var(--weight-medium);
+    margin-bottom: 6px;
+  }
+`;
+
+export const SectionChatComments = styled.div`
+  height: 50vh;
+  position: relative;
+  overflow-y: auto;
+
+  @media (max-height: 800px) {
+    height: 40vh;
+  }
+`;
+
+export const InputChat = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  align-self: stretch;
+  gap: 8px;
+
+  padding: 10px 14px;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+
+  height: 44px;
+  width: 100%;
+
+  border-bottom: 2px solid var(--gray-200);
+  background: var(--gray-50);
+
+  input {
+    background: transparent;
+    border: none;
+  }
+`;
+
+export const ChatSendButton = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 24px;
+  height: 24px;
+  border-radius: 6px;
+  background: transparent;
+  transition: all 0.3s;
+
+  svg {
+    path {
+      stroke: var(--gray-400);
+    }
+  }
+
+  &:hover {
+    transform: scale(1.2);
+    background: var(--success-200);
+
+    svg {
+      path {
+        stroke: var(--success-800);
+      }
+    }
+  }
+`;
