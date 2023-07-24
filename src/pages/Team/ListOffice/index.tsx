b7b62878ff0fa7079716b19d99bcab42fb8f66f0
Fino59 <slipdan@hotmail.com>
2023-07-24 18:23:08 -0300

    Update List Office page with permissions

--- src/pages/Team/ListOffice/index.tsx
+++ src/pages/Team/ListOffice/index.tsx
@@ -1,24 +1,28 @@
-import { useCallback, useState } from 'react';
+/* eslint-disable import-helpers/order-imports */
+// react
+import { useCallback, useEffect, useState } from 'react';
 import { BiPlus, BiSearchAlt } from 'react-icons/bi';
 
+// Api
 import api from '../../../services/api';
 
+// Hooks
 import { useToast } from '../../../hooks/toast';
 import useDebouncedCallback from '../../../hooks/useDebounced';
 import { useFetch } from '../../../hooks/useFetch';
 import useForm from '../../../hooks/useForm';
 
+// Components
 import ButtonDefault from '../../../components/Buttons/ButtonDefault';
 import ButtonTable from '../../../components/Buttons/ButtonTable';
 import HeaderPage from '../../../components/HeaderPage';
 import { InputDefault } from '../../../components/Inputs/InputDefault';
-import { TableDefault } from '../../../components/TableDefault';
+import { Table } from '../../../components/Table';
+import { TableHead } from '../../../components/Table/styles';
 import Alert from '../../../components/Ui/Alert';
 import ModalDefault from '../../../components/Ui/ModalDefault';
-import ScrollAreas from '../../../components/Ui/ScrollAreas';
 import {
   ContainerDefault,
-  ContainerGroupTable,
   ContentDefault,
   FieldDefault,
   FieldGroupFormDefault,
@@ -26,11 +30,23 @@ import {
   SectionDefault
 } from '../../../components/UiElements/styles';
 
+// Styles
+import { PermissionsList, PermissionsTitle, PermissionsWrapper } from './styles';
+
+// Libraries
+import Switch from 'react-switch';
+
 interface OfficeProps {
   function_id: number;
   tenant_id: number;
   function: string;
   description: string;
+}
+
+interface PermissionProps {
+  permission_id: string;
+  name: string;
+  checked: boolean;
 }
 
 export default function ListOffice() {
@@ -53,8 +69,9 @@ export default function ListOffice() {
     (search: string) => setSearch(search),
     700
   );
-
   const { data, fetchData } = useFetch<OfficeProps[]>(`function?=${search}`);
+  const [permissionFor, setPermissionFor] = useState<any[]>([]);
+  const [permissionsData, setPermissionsData] = useState<PermissionProps[]>([]);
 
   const handleOnCancel = useCallback(() => {
     setModal({
@@ -135,6 +152,38 @@ export default function ListOffice() {
     [formData, addToast, fetchData, handleOnCancel, modal]
   );
 
+  const handlePermissions = (id: any) => {
+    if (permissionFor.includes(id)) {
+      setPermissionFor(permissionFor.filter((obj) => obj !== id));
+    } else {
+      setPermissionFor((prevState: any) => [...prevState, id]);
+    }
+  };
+
+  useEffect(() => {
+    const handleGetPermissions = async () => {
+      try {
+        const response = await api.get(`/permissions`);
+
+        const allPermissions: PermissionProps[] = [];
+
+        response.data.result.map((row: any) => {
+          allPermissions.push({
+            permission_id: row.permission_id,
+            name: row.name,
+            checked: false
+          });
+        });
+
+        setPermissionsData(allPermissions);
+      } catch (error) {
+        console.log('log do error getting permissions');
+      }
+    };
+
+    handleGetPermissions();
+  }, []);
+
   return (
     <ContainerDefault>
       <HeaderPage title="Cargos">
@@ -169,23 +218,27 @@ export default function ListOffice() {
             />
           </FieldGroupFormDefault>
         </ContentDefault>
-
-        <ContainerGroupTable style={{ marginTop: '1rem' }}>
-          <ScrollAreas>
-            <TableDefault title="Cargos">
+        <div style={{ margin: '-24px -30px' }}>
+          <Table>
+            <TableHead>
+              <div className="groupTable">
+                <h2>Lista de cargos</h2>
+              </div>
+            </TableHead>
+            <table>
               <thead>
                 <tr style={{ whiteSpace: 'nowrap' }}>
                   <th>ID</th>
                   <th>Cargo</th>
                   <th>Descrição</th>
-                  <th style={{ display: 'grid', placeItems: 'center' }}>-</th>
+                  <th style={{ display: 'grid', placeItems: 'center', color: '#F9FAFB' }}>-</th>
                 </tr>
               </thead>
 
               <tbody>
                 {data?.map((row) => (
                   <tr key={row.function_id}>
-                    <td>{row.function_id}</td>
+                    <td>#{String(row.function_id).padStart(5, '0')}</td>
                     <td>{row.function}</td>
                     <td>{row.description}</td>
                     <td>
@@ -203,9 +256,9 @@ export default function ListOffice() {
                   </tr>
                 ))}
               </tbody>
-            </TableDefault>
-          </ScrollAreas>
-        </ContainerGroupTable>
+            </table>
+          </Table>
+        </div>
       </SectionDefault>
 
       <ModalDefault isOpen={modal.isOpen} title={modal.type} onOpenChange={handleOnCancel}>
@@ -226,6 +279,25 @@ export default function ListOffice() {
               value={formData.description}
             />
           </FieldDefault>
+          <PermissionsWrapper>
+            <PermissionsTitle>Permissões</PermissionsTitle>
+            <PermissionsList>
+              {permissionsData.map((row: PermissionProps, index: number) => (
+                <div className="permission-field" key={index}>
+                  <Switch
+                    onChange={() => handlePermissions(row.permission_id)}
+                    checked={permissionFor.includes(row.permission_id) ? true : false}
+                    uncheckedIcon={false}
+                    checkedIcon={false}
+                    onColor="#0046B5"
+                    width={40}
+                    height={21}
+                  />
+                  {row.name.split('_')[0]} - {row.name.split('_')[1]}
+                </div>
+              ))}
+            </PermissionsList>
+          </PermissionsWrapper>
           <FooterModal style={{ justifyContent: 'flex-end', gap: '16px' }}>
             <ButtonDefault typeButton="dark" isOutline onClick={handleOnCancel}>
               Descartar
--- src/pages/Team/ListOffice/styles.ts
+++ src/pages/Team/ListOffice/styles.ts
@@ -4,3 +4,33 @@ export const Container = styled.div`
   display: flex;
   flex-direction: column;
 `;
+
+export const PermissionsWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const PermissionsTitle = styled.h4`
+  color: var(--gray-700);
+  font-size: var(--text-small-sm);
+  font-weight: var(--weight-medium);
+`;
+
+export const PermissionsList = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+  padding: 22px 18px;
+
+  .permission-field {
+    display: flex;
+    align-items: center;
+    gap: 10px;
+
+    color: var(--gray-600);
+    font-size: var(--text-small-sm);
+    font-weight: var(--weight-regular);
+    line-height: 18px;
+    text-transform: capitalize;
+  }
+`;
