6c17a1cfa4287c37a9705f357ac83cb902235baf
Fino59 <slipdan@hotmail.com>
2023-07-25 18:27:22 -0300

    Update View Tasks page to correct navigate infos

--- src/pages/Tasks/ViewTasks/index.tsx
+++ src/pages/Tasks/ViewTasks/index.tsx
@@ -10,6 +10,7 @@ import TaskTable from '../../../components/Ui/TaskTable';
 // Hooks
 import useDebouncedCallback from '../../../hooks/useDebounced';
 import { useNavigate } from 'react-router-dom';
+import { useFetch } from '../../../hooks/useFetch';
 
 export interface TaskProps {
   date: string;
@@ -40,102 +41,105 @@ export interface ProjectInfo {
 export default function ViewTaskList() {
   const navigate = useNavigate();
   const [searchTerm, setSearchTerm] = useState('');
+  const [selected, setSelected] = useState<number>(1);
+  const [search, setSearch] = useState('');
   const { isLoading, debouncedCallback } = useDebouncedCallback(
-    (search: string) => setSearchTerm(search),
+    (search: string) => setSearch(search),
     700
   );
+  const { data, pages } = useFetch<any[]>(`tasks?search=${search}&page=${selected}`);
 
-  const data = [
-    {
-      date: '2023/07/15',
-      tasks: [
-        {
-          id: '001',
-          projectInfo: {
-            taskTitle: 'Cronograma',
-            month: 'Julho/2023',
-            client: 'G.WIND',
-            type: 'FEE',
-            quantity: 'PACK 8 POSTS/MÊS'
-          },
-          consumedTime: '00:30:00',
-          estimatedTime: '02:00:00',
-          startDate: '2023/06/26',
-          endDate: '2023/07/15',
-          deliveries: '4 produtos',
-          stage: 'Criação',
-          flow: 'CAMPANHA',
-          status: 'progress'
-        },
-        {
-          id: '002',
-          projectInfo: {
-            taskTitle: 'Cronograma',
-            month: 'Agosto/2023',
-            client: 'G.WIND',
-            type: 'FEE',
-            quantity: 'PACK 8 POSTS/MÊS'
-          },
-          consumedTime: '00:00:00',
-          estimatedTime: '02:00:00',
-          startDate: '2023/07/16',
-          endDate: '2023/07/25',
-          deliveries: '4 produtos',
-          stage: 'Criação',
-          flow: 'CAMPANHA',
-          status: 'pending'
-        }
-      ]
-    },
-    {
-      date: '2023/06/02',
-      tasks: [
-        {
-          id: '003',
-          projectInfo: {
-            taskTitle: 'Cronograma',
-            month: 'Agosto/2023',
-            client: 'G.WIND',
-            type: 'FEE',
-            quantity: 'PACK 4 POSTS/MÊS'
-          },
-          consumedTime: '00:00:00',
-          estimatedTime: '02:00:00',
-          startDate: '2023/06/26',
-          endDate: '2023/07/15',
-          deliveries: '4 produtos',
-          stage: 'Criação',
-          flow: 'CAMPANHA',
-          status: 'pending'
-        },
-        {
-          id: '004',
-          projectInfo: {
-            taskTitle: 'Cronograma',
-            month: 'Agosto/2023',
-            client: 'G.WIND',
-            type: 'FEE',
-            quantity: 'PACK 4 POSTS/MÊS'
-          },
-          consumedTime: '00:00:00',
-          estimatedTime: '02:00:00',
-          startDate: '2023/07/16',
-          endDate: '2023/07/25',
-          deliveries: '4 produtos',
-          stage: 'Criação',
-          flow: 'CAMPANHA',
-          status: 'pending'
-        }
-      ]
-    }
-  ];
+  // const data = [
+  //   {
+  //     date: '2023/07/15',
+  //     tasks: [
+  //       {
+  //         id: '001',
+  //         projectInfo: {
+  //           taskTitle: 'Cronograma',
+  //           month: 'Julho/2023',
+  //           client: 'G.WIND',
+  //           type: 'FEE',
+  //           quantity: 'PACK 8 POSTS/MÊS'
+  //         },
+  //         consumedTime: '00:30:00',
+  //         estimatedTime: '02:00:00',
+  //         startDate: '2023/06/26',
+  //         endDate: '2023/07/15',
+  //         deliveries: '4 produtos',
+  //         stage: 'Criação',
+  //         flow: 'CAMPANHA',
+  //         status: 'progress'
+  //       },
+  //       {
+  //         id: '002',
+  //         projectInfo: {
+  //           taskTitle: 'Cronograma',
+  //           month: 'Agosto/2023',
+  //           client: 'G.WIND',
+  //           type: 'FEE',
+  //           quantity: 'PACK 8 POSTS/MÊS'
+  //         },
+  //         consumedTime: '00:00:00',
+  //         estimatedTime: '02:00:00',
+  //         startDate: '2023/07/16',
+  //         endDate: '2023/07/25',
+  //         deliveries: '4 produtos',
+  //         stage: 'Criação',
+  //         flow: 'CAMPANHA',
+  //         status: 'pending'
+  //       }
+  //     ]
+  //   },
+  //   {
+  //     date: '2023/06/02',
+  //     tasks: [
+  //       {
+  //         id: '003',
+  //         projectInfo: {
+  //           taskTitle: 'Cronograma',
+  //           month: 'Agosto/2023',
+  //           client: 'G.WIND',
+  //           type: 'FEE',
+  //           quantity: 'PACK 4 POSTS/MÊS'
+  //         },
+  //         consumedTime: '00:00:00',
+  //         estimatedTime: '02:00:00',
+  //         startDate: '2023/06/26',
+  //         endDate: '2023/07/15',
+  //         deliveries: '4 produtos',
+  //         stage: 'Criação',
+  //         flow: 'CAMPANHA',
+  //         status: 'pending'
+  //       },
+  //       {
+  //         id: '004',
+  //         projectInfo: {
+  //           taskTitle: 'Cronograma',
+  //           month: 'Agosto/2023',
+  //           client: 'G.WIND',
+  //           type: 'FEE',
+  //           quantity: 'PACK 4 POSTS/MÊS'
+  //         },
+  //         consumedTime: '00:00:00',
+  //         estimatedTime: '02:00:00',
+  //         startDate: '2023/07/16',
+  //         endDate: '2023/07/25',
+  //         deliveries: '4 produtos',
+  //         stage: 'Criação',
+  //         flow: 'CAMPANHA',
+  //         status: 'pending'
+  //       }
+  //     ]
+  //   }
+  // ];
 
   const handleFilters = () => {
     console.log('log do filters on task');
   };
 
-  const handleNavigateTask = (id: any) => {
-    navigate(`/tarefa/${id}`);
+  const handleNavigateTask = (task: any) => {
+    navigate(`/tarefa/${task.task_id}`, { state: task });
   };
 
   return (
@@ -143,7 +147,7 @@ export default function ViewTaskList() {
       <HeaderPage title="Suas tarefas" />
 
       <TaskTable
-        data={data}
+        data={data ? data : []}
         loading={isLoading}
         searchInput={(value: any) => {
           setSearchTerm(value);
