a578a3cc9f014ba2a6b773dd9c693dc6e880b94b
Fino59 <slipdan@hotmail.com>
2023-07-27 18:30:01 -0300

    Create Delivery Table component

---
+++ src/components/DeliveryTable/index.tsx
@@ -0,0 +1,195 @@
+/* eslint-disable import-helpers/order-imports */
+// React
+import { useEffect } from 'react';
+
+// Icons
+import { BiFilter, BiSearchAlt } from 'react-icons/bi';
+import { FiFlag } from 'react-icons/fi';
+import { IconContext } from 'react-icons';
+import { IconText } from '../../assets/icons';
+
+// Utils
+import { convertToMilliseconds } from '../../utils/convertToMilliseconds';
+
+// Styles
+import { Flag } from '../../pages/Tasks/TaskList/styles';
+import {
+  DeliveryContainer,
+  DeliveryDate,
+  DeliveryDateWrapper,
+  DeliveryFilter,
+  DeliveriesTable
+} from './styles';
+
+// Components
+import ButtonDefault from '../Buttons/ButtonDefault';
+import { InputDefault } from '../Inputs/InputDefault';
+
+// Libraries
+import ProgressBar from '../Ui/ProgressBar';
+import moment from 'moment';
+import 'moment/dist/locale/pt-br';
+
+interface DeliveryProps {
+  data: any;
+  loading: boolean;
+  searchInput: any;
+  searchInfo: string;
+  addFilter: any;
+  taskSelected: any;
+}
+
+export default function DeliveryTable({
+  data,
+  loading,
+  searchInput,
+  searchInfo,
+  addFilter,
+  taskSelected
+}: DeliveryProps) {
+  const arrayData = Object.entries(data);
+
+  const handleGoToProducts = (taskInfos: any, taskIndex: any) => {
+    const allTaskInfo = {
+      task_index: taskIndex,
+      task: taskInfos
+    };
+    taskSelected(allTaskInfo);
+  };
+
+  return (
+    <DeliveryContainer>
+      <DeliveryFilter>
+        <InputDefault
+          label=""
+          name="search"
+          placeholder="Buscar..."
+          onChange={(event) => searchInput(event.target.value)}
+          value={searchInfo}
+          icon={BiSearchAlt}
+          isLoading={loading}
+          className="search-field"
+        />
+
+        {/* <ButtonDefault typeButton="lightWhite" isOutline onClick={addFilter}>
+          <BiFilter />
+          Ordenar por
+        </ButtonDefault> */}
+      </DeliveryFilter>
+
+      {arrayData?.map((row: any, index: number) => (
+        <DeliveryDateWrapper key={index}>
+          <DeliveryDate>{moment(row[0]).format('DD/MM/YYYY')}</DeliveryDate>
+          <DeliveriesTable>
+            <table>
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>Tarefas</th>
+                  <th>Tempo consumido</th>
+                  <th>Tempo estimado</th>
+                  <th>Data inicial</th>
+                  <th>Data final</th>
+                  <th>Entregas</th>
+                  <th>Etapa</th>
+                  <th>Status</th>
+                </tr>
+              </thead>
+              <tbody>
+                {row[1].map((task: any, index: number) => (
+                  <tr
+                    key={task.task_id}
+                    style={{ cursor: 'pointer' }}
+                    onClick={() => handleGoToProducts(task, index + 1)}
+                  >
+                    <td>
+                      <div className="id-column">
+                        #{String(task.task_id).padStart(5, '0')} | {task.id}
+                      </div>
+                    </td>
+                    <td>
+                      <div className="column info">
+                        <div>
+                          <IconText /> {task.title}
+                        </div>
+                        <span>
+                          {task.tenant} / {task.project_category} | {task.product_period}
+                        </span>
+                      </div>
+                    </td>
+                    <td>
+                      <span style={{ marginBottom: '4px', display: 'block' }}>
+                        {task.timeConsumed}
+                      </span>
+                      <ProgressBar
+                        totalHours={convertToMilliseconds(task.totalTime)}
+                        restHours={convertToMilliseconds(task.timeConsumed)}
+                      />
+                    </td>
+                    <td>
+                      <div className="flag-info">
+                        <Flag
+                          style={{ textAlign: 'center' }}
+                          className={task.status === 'true' ? 'flagged' : ''}
+                        >
+                          {task.status === 'true' ? (
+                            <IconContext.Provider
+                              value={{ color: '#F04438', className: 'global-class-name' }}
+                            >
+                              <FiFlag />
+                            </IconContext.Provider>
+                          ) : (
+                            <IconContext.Provider
+                              value={{ color: '#667085', className: 'global-class-name' }}
+                            >
+                              <FiFlag />
+                            </IconContext.Provider>
+                          )}
+                        </Flag>
+                        {task.totalTime}
+                      </div>
+                    </td>
+                    <td style={{ textTransform: 'capitalize' }}>
+                      {moment(task.copywriting_date_end).format('DD/MMM/YYYY')}
+                    </td>
+                    <td style={{ textTransform: 'capitalize' }}>
+                      {moment(task.creation_date_end).format('DD/MMM/YYYY')}
+                    </td>
+                    <td>
+                      {task?.entregas?.length <= 1
+                        ? `${task?.entregas?.length} produto`
+                        : `${task?.entregas?.length} produtos`}
+                    </td>
+                    <td>
+                      <div className="column">
+                        {task.step}???
+                        <span>Fluxo: {task.flow}</span>
+                      </div>
+                    </td>
+                    <td>
+                      <div
+                        className={
+                          task.status === 'progress'
+                            ? 'status progress'
+                            : task.status === 'finished'
+                            ? 'status finished'
+                            : 'status'
+                        }
+                      >
+                        {task.status === 'progress'
+                          ? 'Em progresso'
+                          : task.status === 'finished'
+                          ? 'Concluída'
+                          : 'Pendente'}
+                      </div>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </DeliveriesTable>
+        </DeliveryDateWrapper>
+      ))}
+    </DeliveryContainer>
+  );
+}
