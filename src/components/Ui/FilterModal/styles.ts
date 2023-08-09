81e4f4cced6a23ce03a10aa1a7021460431e383b
Fino59 <slipdan@hotmail.com>
2023-08-09 18:22:38 -0300

    Create Filter Modal styles component

---
+++ src/components/Ui/FilterModal/styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const CloseButton = styled.button`
+  width: 24px;
+  height: 24px;
+
+  position: absolute;
+  top: 0;
+  right: 0;
+  margin-right: 44px;
+  margin-top: 32px;
+  background: var(--background-primary);
+`;
