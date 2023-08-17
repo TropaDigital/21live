b74c2fee1d1b83cefac31f4e78cd2297c7d3df02
Fino59 <slipdan@hotmail.com>
2023-08-17 18:24:47 -0300

    feat: Update List Requests page changing to real data

--- src/pages/Requests/ListRequests/index.tsx
+++ src/pages/Requests/ListRequests/index.tsx
@@ -65,7 +65,7 @@ export default function Requests() {
     fromDate: '',
     toDate: ''
   });
-  const { data, pages } = useFetch(`solicitacoes?search=${search}&page=${selected}`);
+  const { data, pages } = useFetch<any[]>(`ticket?search=${search}&page=${selected}`);
 
   const handleViewRequest = (request: any) => {
     console.log('log request to view', request);
@@ -91,58 +91,58 @@ export default function Requests() {
   };
 
   // FakeData
-  const requests: RequestsProps[] = [
-    {
-      id: 1,
-      title: 'Request 1',
-      format: 'Type A',
-      status: 'Pending',
-      user: 'User A',
-      unit: 'Unit X',
-      startDate: '2023-08-07',
-      finishDate: '2023-08-12'
-    },
-    {
-      id: 2,
-      title: 'Request 2',
-      format: 'Type B',
-      status: 'finished',
-      user: 'User B',
-      unit: 'Unit Y',
-      startDate: '2023-08-08',
-      finishDate: '2023-08-15'
-    },
-    {
-      id: 3,
-      title: 'Request 3',
-      format: 'Type C',
-      status: 'progress',
-      user: 'User C',
-      unit: 'Unit Z',
-      startDate: '2023-08-09',
-      finishDate: '2023-08-14'
-    },
-    {
-      id: 4,
-      title: 'Request 4',
-      format: 'Type A',
-      status: 'Pending',
-      user: 'User D',
-      unit: 'Unit X',
-      startDate: '2023-08-10',
-      finishDate: '2023-08-17'
-    },
-    {
-      id: 5,
-      title: 'Request 5',
-      format: 'Type B',
-      status: 'finished',
-      user: 'User E',
-      unit: 'Unit Y',
-      startDate: '2023-08-11',
-      finishDate: '2023-08-16'
-    }
-  ];
+  // const requests: RequestsProps[] = [
+  //   {
+  //     id: 1,
+  //     title: 'Request 1',
+  //     format: 'Type A',
+  //     status: 'Pending',
+  //     user: 'User A',
+  //     unit: 'Unit X',
+  //     startDate: '2023-08-07',
+  //     finishDate: '2023-08-12'
+  //   },
+  //   {
+  //     id: 2,
+  //     title: 'Request 2',
+  //     format: 'Type B',
+  //     status: 'finished',
+  //     user: 'User B',
+  //     unit: 'Unit Y',
+  //     startDate: '2023-08-08',
+  //     finishDate: '2023-08-15'
+  //   },
+  //   {
+  //     id: 3,
+  //     title: 'Request 3',
+  //     format: 'Type C',
+  //     status: 'progress',
+  //     user: 'User C',
+  //     unit: 'Unit Z',
+  //     startDate: '2023-08-09',
+  //     finishDate: '2023-08-14'
+  //   },
+  //   {
+  //     id: 4,
+  //     title: 'Request 4',
+  //     format: 'Type A',
+  //     status: 'Pending',
+  //     user: 'User D',
+  //     unit: 'Unit X',
+  //     startDate: '2023-08-10',
+  //     finishDate: '2023-08-17'
+  //   },
+  //   {
+  //     id: 5,
+  //     title: 'Request 5',
+  //     format: 'Type B',
+  //     status: 'finished',
+  //     user: 'User E',
+  //     unit: 'Unit Y',
+  //     startDate: '2023-08-11',
+  //     finishDate: '2023-08-16'
+  //   }
+  // ];
 
   return (
     <ContainerDefault>
@@ -186,32 +186,32 @@ export default function Requests() {
             </thead>
 
             <tbody>
-              {requests.map((row: RequestsProps, index: number) => (
+              {data?.map((row: any, index: number) => (
                 <tr key={index}>
-                  <td>#{String(row.id).padStart(5, '0')}</td>
+                  <td>#{String(row.ticket_id).padStart(5, '0')}</td>
                   <td>{row.title}</td>
-                  <td>{row.format}</td>
+                  <td>{row.media_name}</td>
                   <td>
                     <div
                       className={
                         row.status === 'progress'
                           ? 'status progress'
-                          : row.status === 'finished'
+                          : row.status === 'Entregue'
                           ? 'status finished'
                           : 'status'
                       }
                     >
                       {row.status === 'progress'
                         ? 'Em progresso'
-                        : row.status === 'finished'
+                        : row.status === 'Entregue'
                         ? 'Concluída'
                         : 'Pendente'}
                     </div>
                   </td>
-                  <td>{row.user}</td>
+                  <td>{row.user_name}</td>
                   <td>{row.unit}</td>
-                  <td>{moment(row.startDate).format('DD/MM/YYYY')}</td>
-                  <td>{moment(row.finishDate).format('DD/MM/YYYY')}</td>
+                  <td>{moment(row.created).format('DD/MM/YYYY')}</td>
+                  <td>{moment(row.finished).format('DD/MM/YYYY')}</td>
                   <td>
                     <div className="fieldTableClients">
                       <ButtonTable typeButton="view" onClick={() => handleViewRequest(row)} />
