9a54da445d241015e2d8b57f4be8634d13c94f76
Fino59 <slipdan@hotmail.com>
2023-08-30 18:22:11 -0300

    feat: Create Schedule User component folder with index and styles

---
+++ src/components/ScheduleUser/index.tsx
@@ -0,0 +1,11 @@
+import { useEffect, useRef, useState } from 'react';
+
+import { ScheduleWrapper } from './styles';
+
+export default function ScheduleUser() {
+  return (
+    <ScheduleWrapper>
+      <div>teste</div>
+    </ScheduleWrapper>
+  );
+}
---
+++ src/components/ScheduleUser/styles.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const ScheduleWrapper = styled.div`
+  width: 800px;
+  background-color: red;
+`;
