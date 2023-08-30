ff439c3242c89d0f9f242e4d786341bf77ef6058
Fino59 <slipdan@hotmail.com>
2023-08-30 11:30:37 -0300

    feat: Update Info Files of Projects with loading for files

--- src/pages/Projects/ComponentSteps/InfoFiles.tsx
+++ src/pages/Projects/ComponentSteps/InfoFiles.tsx
@@ -32,6 +32,7 @@ export default function InfoFiles({
           isDisabed={isDisabed}
           loading={loading}
           setLoading={setLoading}
+          folderInfo="projects"
         />
       </FieldDefault>
     </ContainerSteps>
