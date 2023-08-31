24a5631b5a02596f52e61c3eb4bc9e754f1930f7
Fino59 <slipdan@hotmail.com>
2023-08-31 18:32:29 -0300

    feat: Update Schedule User with table of users

--- src/components/ScheduleUser/index.tsx
+++ src/components/ScheduleUser/index.tsx
@@ -1,11 +1,240 @@
 import { useEffect, useRef, useState } from 'react';
 
-import { ScheduleWrapper } from './styles';
+import {
+  ArrowButton,
+  HoursTable,
+  HoursTitle,
+  ScheduleDate,
+  ScheduleSelectUser,
+  ScheduleSubtitle,
+  ScheduleTable,
+  ScheduleWrapper,
+  SubtitleInfo,
+  UserCard,
+  UserFields,
+  UserInfosCard,
+  UserTable
+} from './styles';
+import { IconClose } from '../../assets/icons';
+import { BsArrowLeft, BsArrowRight } from 'react-icons/bs';
+import { InputDefault } from '../Inputs/InputDefault';
+import ButtonDefault from '../Buttons/ButtonDefault';
+import { CheckboxDefault } from '../Inputs/CheckboxDefault';
+import { HourDate } from '../../utils/dataDefault';
+
+interface Task {
+  task_id?: string | null;
+  start: string;
+  end: string;
+  type: 'job' | 'pause';
+  title?: string;
+}
+
+interface WorkSchedule {
+  start_work: string;
+  end_work: string;
+}
+
+interface UserData {
+  user_id: string;
+  name: string;
+  function: string;
+  work: WorkSchedule;
+  agenda: Task[];
+}
 
 export default function ScheduleUser() {
+  const [hours, setHours] = useState<number>(0);
+  const [minutes, setMinutes] = useState<number>(0);
+
+  const dataTest: UserData[] = [
+    {
+      user_id: '15853',
+      name: 'Robert Fox',
+      function: 'Gerente de Projeto',
+      work: {
+        start_work: '09:30',
+        end_work: '17:30'
+      },
+      agenda: [
+        {
+          task_id: '170',
+          start: '2023-08-31 09:30',
+          end: '2023-08-31 13:30',
+          type: 'job',
+          title: 'Tarefa 1'
+        },
+        {
+          start: '2023-08-31 13:30',
+          end: '2023-08-31 14:00',
+          type: 'pause'
+        },
+        {
+          task_id: '170',
+          start: '2023-08-31 14:00',
+          end: '2023-08-31 16:00',
+          type: 'job',
+          title: 'Tarefa 2'
+        },
+        {
+          start: '2023-08-31 16:00',
+          end: '2023-08-31 16:30',
+          type: 'pause'
+        },
+        {
+          task_id: '170',
+          start: '2023-08-31 16:30',
+          end: '2023-08-31 17:30',
+          type: 'job',
+          title: 'Tarefa 3'
+        }
+      ]
+    },
+    {
+      user_id: '15853',
+      name: 'Darlene Robertson',
+      function: 'Gerente de Projeto',
+      work: {
+        start_work: '10:00',
+        end_work: '19:00'
+      },
+      agenda: [
+        {
+          task_id: '171',
+          start: '2023-08-31 10:30',
+          end: '2023-08-31 13:30',
+          type: 'job',
+          title: 'Tarefa 1'
+        },
+        {
+          start: '2023-08-31 13:30',
+          end: '2023-08-31 14:00',
+          type: 'pause'
+        },
+        {
+          task_id: '170',
+          start: '2023-08-31 14:00',
+          end: '2023-08-31 16:00',
+          type: 'job',
+          title: 'Tarefa 2'
+        },
+        {
+          start: '2023-08-31 16:00',
+          end: '2023-08-31 16:30',
+          type: 'pause'
+        },
+        {
+          task_id: '170',
+          start: '2023-08-31 16:30',
+          end: '2023-08-31 17:30',
+          type: 'job',
+          title: 'tarefa 3'
+        }
+      ]
+    }
+  ];
+
   return (
     <ScheduleWrapper>
-      <div>teste</div>
+      <ScheduleSubtitle>
+        <SubtitleInfo>
+          <div className="title">Título da tarefa:</div>
+          <div className="info">Plano de comunicação</div>
+        </SubtitleInfo>
+        <div>•</div>
+        <SubtitleInfo>
+          <div className="title">Tempo estimado:</div>
+          <div className="info">02:00:00</div>
+        </SubtitleInfo>
+
+        <button className="close">
+          <IconClose />
+        </button>
+      </ScheduleSubtitle>
+
+      <ScheduleDate>
+        <ArrowButton>
+          <BsArrowLeft />
+        </ArrowButton>
+
+        <div className="date">16 de Agosto</div>
+
+        <ArrowButton>
+          <BsArrowRight />
+        </ArrowButton>
+      </ScheduleDate>
+
+      {/* Aqui vai a tabela */}
+      <ScheduleTable>
+        <UserTable>
+          <div className="user-title">Usuário</div>
+          {dataTest.map((row: UserData) => (
+            <UserCard key={row.user_id}>
+              <CheckboxDefault
+                label=""
+                name="necessary_responsible"
+                onChange={() => ''}
+                checked={false}
+              />
+              <UserInfosCard>
+                <div className="user-name">{row.name}</div>
+                <div className="working-hours">
+                  Jornada:{' '}
+                  <span>
+                    {row.work.start_work} - {row.work.end_work}
+                  </span>{' '}
+                </div>
+              </UserInfosCard>
+            </UserCard>
+          ))}
+        </UserTable>
+
+        <HoursTable>
+          <HoursTitle>
+            {HourDate.map((row: any, index: number) => (
+              <div className="hours" key={index}>
+                {row.hour}
+              </div>
+            ))}
+          </HoursTitle>
+          <div style={{ height: '72px' }}></div>
+        </HoursTable>
+      </ScheduleTable>
+
+      <ScheduleSelectUser>
+        <UserFields>
+          <InputDefault
+            label="Hora início"
+            name="hours_creation"
+            onChange={(value) => setHours(Number(value))}
+            value={hours}
+            type="number"
+            min="0"
+            max="23"
+            step="1"
+            required
+          />
+          :
+          <InputDefault
+            label="Hora final"
+            name="minutes_creation"
+            onChange={(value) => setMinutes(Number(value))}
+            value={minutes}
+            type="number"
+            min="0"
+            max="59"
+            step="1"
+            required
+          />
+        </UserFields>
+
+        <UserFields>
+          <div className="selectedUser">
+            Usuário selecionado: <strong>Cara da gestão</strong>
+          </div>
+          <ButtonDefault typeButton="blocked">Alocar usuário</ButtonDefault>
+        </UserFields>
+      </ScheduleSelectUser>
     </ScheduleWrapper>
   );
 }
--- src/components/ScheduleUser/styles.ts
+++ src/components/ScheduleUser/styles.ts
@@ -1,6 +1,205 @@
 import styled from 'styled-components';
 
 export const ScheduleWrapper = styled.div`
-  width: 800px;
-  background-color: red;
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+
+  width: 85vw;
+`;
+
+export const ScheduleSubtitle = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 12px;
+
+  position: relative;
+
+  .close {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 32px;
+    height: 32px;
+    background-color: transparent;
+
+    position: absolute;
+    top: -40px;
+    right: 0;
+    transition: all 0.3s;
+
+    svg {
+      width: 32px;
+      height: 32px;
+      path {
+        stroke: var(--gray-600);
+      }
+    }
+
+    &:hover {
+      transform: scale(1.3);
+
+      svg {
+        path {
+          stroke: var(--Danger);
+        }
+      }
+    }
+  }
+`;
+
+export const SubtitleInfo = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+
+  .title {
+    color: var(--gray-600);
+    font-size: var(--text-small-md);
+    font-weight: var(--weight-regular);
+  }
+
+  .info {
+    color: var(--gray-900);
+    font-size: var(--text-small-md);
+    font-weight: var(--weight-medium);
+  }
+`;
+
+export const ScheduleDate = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  height: 36px;
+
+  .date {
+    color: var(--gray-700);
+    font-size: var(--text-small-xl);
+    font-weight: var(--weight-semibold);
+  }
+`;
+
+export const ArrowButton = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  padding: 8px;
+
+  border-radius: 4px;
+  border: 1px solid var(--gray-200, #eaecf0);
+  background: transparent;
+
+  /* Shadow/xs */
+  box-shadow: 0px 1px 2px 0px rgba(16, 24, 40, 0.05);
+`;
+
+export const ScheduleSelectUser = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  height: 68px;
+`;
+
+export const UserFields = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 12px;
+
+  .selectedUser {
+    color: var(--gray-700);
+    font-size: var(--text-small-sm);
+    font-weight: var(--weight-medium);
+  }
+`;
+
+export const ScheduleTable = styled.div`
+  display: flex;
+  align-items: flex-start;
+
+  border-radius: 4px;
+  border: 1px solid var(--gray-200, #eaecf0);
+  background: var(--White, #fff);
+`;
+
+export const UserTable = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  width: 211px;
+
+  box-shadow: 8px 0 10px -1px #eaecf0;
+
+  .user-title {
+    padding: 12px 24px;
+    height: 44px;
+
+    color: var(--gray-500);
+    font-size: var(--text-small-xs);
+    font-weight: var(--weight-medium);
+
+    border-right: 2px solid var(--gray-200);
+    border-bottom: 2px solid var(--gray-200);
+  }
+`;
+
+export const UserCard = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 12px;
+
+  width: 211px;
+  height: 72px;
+
+  border-right: 2px solid var(--gray-200);
+  border-bottom: 2px solid var(--gray-200);
+`;
+
+export const UserInfosCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+
+  .user-name {
+    color: var(--gray-700);
+    font-size: var(--text-small-sm);
+    font-weight: var(--weight-semibold);
+  }
+
+  .working-hours {
+    color: var(--gray-500);
+    font-size: var(--text-small-xs);
+    font-weight: var(--weight-regular);
+
+    span {
+      color: var(--gray-600);
+      font-weight: var(--weight-semibold);
+    }
+  }
+`;
+
+export const HoursTable = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  width: calc(100% - 211px);
+
+  overflow-y: hidden;
+  overflow-x: auto;
+`;
+
+export const HoursTitle = styled.div`
+  display: flex;
+  align-items: center;
+  border-bottom: 2px solid var(--gray-200);
+  height: 44px;
+
+  .hours {
+    width: 84px;
+
+    padding: 12px 24px;
+  }
 `;
