a2f5081c6b97080a6728bcf0a90f494c862d3b3b
Fino59 <slipdan@hotmail.com>
2023-09-01 18:19:21 -0300

    feat: Create Schedule User helpers folder

---
+++ src/components/ScheduleUser/helpers/epg.ts
@@ -0,0 +1,24 @@
+export const epg = [
+  {
+    id: '6f3caa7f-5b11-4edb-998e-80d4baa03373',
+    description:
+      "Bounty hunter Boba Fett & mercenary Fennec Shand navigate the underworld when they return to Tatooine to claim Jabba the Hutt's old turf.",
+    title: 'Criação de Post',
+    isYesterday: false,
+    since: '2022-10-18T00:05:00',
+    till: '2022-10-18T01:05:00',
+    type: 'job',
+    channelUuid: '16fdfefe-e466-4090-bc1a-57c43937f826'
+  },
+  {
+    id: '6f3caa7f-5b11-4edb-999e-80d4baa03373',
+    description:
+      "Bounty hunter Boba Fett & mercenary Fennec Shand navigate the underworld when they return to Tatooine to claim Jabba the Hutt's old turf.",
+    title: 'Reunião',
+    isYesterday: false,
+    since: '2022-10-18T03:00:00',
+    till: '2022-10-18T04:00:00',
+    type: 'pause',
+    channelUuid: '06d3366b-9ec2-46e8-a741-df3ee1abeed7'
+  }
+];
