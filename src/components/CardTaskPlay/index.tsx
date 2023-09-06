76a83ae5a96f32417d5eddcfa646e2e1a06d459e
Fino59 <slipdan@hotmail.com>
2023-09-06 18:24:28 -0300

    feat: Create Card Task Play component

---
+++ src/components/CardTaskPlay/index.tsx
@@ -0,0 +1,108 @@
+// React
+import { useState, useEffect } from 'react';
+
+import {
+  CardTitle,
+  CardWrapper,
+  EstimatedTime,
+  PlayPauseButton,
+  PlayTimer,
+  StopWatchTimer
+} from './styles';
+import { IconPlay } from '../../assets/icons';
+import { IoMdPause } from 'react-icons/io';
+
+interface CardTaskPlayProps {
+  cardTitle: string;
+  dataTime?: any;
+  isPlayingTime: () => void;
+  taskIsFinished?: boolean;
+  elapsedTimeBack?: any;
+}
+
+export default function CardTaskPlay({
+  dataTime,
+  cardTitle,
+  elapsedTimeBack,
+  taskIsFinished,
+  isPlayingTime
+}: CardTaskPlayProps) {
+  const [startTime, setStartTime] = useState<number | null>(null);
+  const [elapsedTime, setElapsedTime] = useState<number>(0);
+  console.log('log do elapsedtime no TASKPLAY', elapsedTimeBack);
+
+  useEffect(() => {
+    if (elapsedTimeBack !== 0 && elapsedTimeBack !== undefined) {
+      setElapsedTime(elapsedTimeBack);
+      setStartTime(Date.now());
+      // isPlayingTime(true);
+    }
+  }, [elapsedTimeBack]);
+
+  useEffect(() => {
+    let intervalId: NodeJS.Timeout;
+
+    if (startTime !== null && !taskIsFinished) {
+      intervalId = setInterval(() => {
+        const now = Date.now();
+        setElapsedTime((prevElapsedTime) => prevElapsedTime + now - startTime);
+        setStartTime(now);
+      }, 1000);
+    } else {
+      setStartTime(null);
+    }
+
+    return () => {
+      if (intervalId) clearInterval(intervalId);
+    };
+  }, [startTime, elapsedTime]);
+
+  const handleStartStop = () => {
+    if (startTime === null) {
+      setStartTime(Date.now());
+      isPlayingTime();
+    } else {
+      setStartTime(null);
+      isPlayingTime();
+    }
+  };
+
+  const formatTime = (time: number): string => {
+    const hours = Math.floor(time / 3600000);
+    const minutes = Math.floor((time % 3600000) / 60000);
+    const seconds = Math.floor((time % 60000) / 1000);
+
+    return `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(
+      seconds
+    ).padStart(2, '0')}`;
+  };
+
+  return (
+    <CardWrapper>
+      <CardTitle>{cardTitle}</CardTitle>
+      <PlayTimer>
+        {startTime === null && (
+          <PlayPauseButton onClick={handleStartStop} className="stop">
+            <IconPlay />
+          </PlayPauseButton>
+        )}
+        {startTime !== null && (
+          <PlayPauseButton onClick={handleStartStop} className="play">
+            <IoMdPause />
+          </PlayPauseButton>
+        )}
+        <StopWatchTimer className={!startTime ? 'stopped' : 'running'}>
+          {formatTime(elapsedTime)}
+          {/* {('0' + Math.floor((time / 3600000) % 60)).slice(-2)}:
+                {('0' + Math.floor((time / 60000) % 60)).slice(-2)}:
+                {('0' + Math.floor((time / 1000) % 60)).slice(-2)} */}
+          {/* Milémisimos de segundos */}
+          {/* {('0' + Math.floor((time / 10) % 100)).slice(-2)} */}
+        </StopWatchTimer>
+      </PlayTimer>
+      <EstimatedTime>
+        Tempo estimado: <span>{dataTime}</span>
+      </EstimatedTime>
+    </CardWrapper>
+  );
+}
---
+++ src/components/CardTaskPlay/styles.ts
@@ -0,0 +1,97 @@
+import styled from 'styled-components';
+
+export const CardWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+
+  min-width: 264px;
+  width: '264px';
+  height: 166px;
+
+  background: var(--background-primary);
+  border: 1px solid var(--gray-200);
+  border-radius: 12px;
+
+  padding: 24px;
+`;
+
+export const CardTitle = styled.div`
+  color: var(--gray-900);
+  font-size: var(--text-small-xl);
+  font-weight: var(--weight-semibold);
+  line-height: 30px;
+`;
+
+export const PlayTimer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+`;
+
+export const PlayPauseButton = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  width: 32px;
+  height: 32px;
+  padding: 4px;
+
+  border-radius: 4px;
+
+  transition: ease-in-out 0.3s;
+
+  cursor: pointer;
+
+  &.play {
+    background-color: var(--success-500);
+  }
+
+  &.stop {
+    background-color: var(--primary);
+  }
+
+  svg {
+    path {
+      fill: var(--light);
+    }
+  }
+
+  &:hover {
+    transform: scale(1.1);
+  }
+`;
+
+export const StopWatchTimer = styled.div`
+  display: flex;
+  align-items: center;
+  width: 92px;
+  height: 32px;
+
+  font-size: var(--text-small-xl);
+  font-weight: var(--weight-semibold);
+
+  &.stopped {
+    color: var(--primary);
+  }
+
+  &.running {
+    color: var(--success-500);
+  }
+`;
+
+export const EstimatedTime = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 7px;
+
+  color: var(--gray-500);
+  font-size: var(--text-small-md);
+  font-weight: var(--weight-medium);
+
+  span {
+    color: var(--gray-900);
+    font-weight: var(--weight-bold);
+  }
+`;
