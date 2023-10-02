66e06579ae9775dbd707014ca2759887b15f3e70
Danilo Fontes <slipdan@hotmail.com>
2023-10-02 19:31:38 -0300

    feat: Update Card Welcome Dash component

--- src/components/Cards/CardWelcomeDash/styles.ts
+++ src/components/Cards/CardWelcomeDash/styles.ts
@@ -1,39 +1,28 @@
 import styled from 'styled-components';
+
 export const CardWellcomeDash = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  
-  background: var(--degrade-blue);
-  border-radius: 10px;
-  
+
   position: relative;
 
   .infoCardWellcome {
     display: flex;
     flex-direction: column;
     align-content: center;
-    margin-left: 2rem;
 
     h1 {
-      font-size: 2.5rem;
-      font-weight: 700;
-      color: #fff;
-
-      text-transform: capitalize;
+      font-size: var(--text-headline-sm);
+      font-weight: var(--weight-semibold);
+      color: var(--gray-900);
+      line-height: 32px;
     }
 
     span {
-      font-size: 0.875rem;
-      font-weight: 400;
-      color: #fff;
+      font-size: var(--text-small-md);
+      font-weight: var(--weight-regular);
+      color: var(--gray-500);
     }
-  }
-
-  .WellcomeImage {
-    display: grid;
-    place-items: center;
-
-    margin-left: 20px;
   }
 `;
