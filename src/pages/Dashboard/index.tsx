00028330f7f37b19f2549640300ad75fdf004272
Danilo Fontes <slipdan@hotmail.com>
2023-10-03 17:59:13 -0300

    feat: Update Dashboard page with new infos

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -7,40 +7,130 @@ import ChartDonut from '../../components/GraphicsChart/ChartDonut';
 import { TableDefault } from '../../components/TableDefault';
 import { ContainerGroupTable, SectionDefault } from '../../components/UiElements/styles';
 
-import { Container } from './styles';
+import {
+  Container,
+  JobStatus,
+  MensalReport,
+  ServicePerformance,
+  UserCardService,
+  UserInfo
+} from './styles';
 
 export default function Dashboard() {
   const { user } = useAuth();
 
+  const dataStatusAll = [
+    {
+      name: 'Entregue',
+      pv: 378,
+      fill: '#00A063'
+    },
+    {
+      name: 'Aprovação',
+      pv: 232,
+      fill: '#0098FF'
+    },
+    {
+      name: 'Criação',
+      pv: 262,
+      fill: '#0045B5'
+    },
+    {
+      name: 'Cancelado',
+      pv: 105,
+      fill: '#D92D20'
+    },
+    {
+      name: 'Pendente',
+      pv: 48,
+      fill: '#FDB022'
+    }
+  ];
+
   const dataDahs = [
     {
       id: 1,
-      name: 'A',
-      value: 20,
-      fill: '#8269B2',
-      currency: 120.0,
+      name: 'Metso',
+      value: 45,
+      fill: '#59B7FF',
+      currency: 20.0,
       isDonut: false,
       isPadded: false
     },
     {
       id: 2,
-      name: 'B',
-      value: 25,
-      fill: '#37C3FF',
-      currency: 80.0,
+      name: 'CNH',
+      value: 28,
+      fill: '#0045B5',
+      currency: 20.0,
       isDonut: true,
       isPadded: false
     },
     {
       id: 3,
-      name: 'C',
-      value: 35,
-      fill: '#93E088',
+      name: 'Emerson',
+      value: 20,
+      fill: '#0077E6',
+      currency: 20.0,
+      isDonut: true,
+      isPadded: true
+    },
+    {
+      id: 4,
+      name: 'Takao',
+      value: 15,
+      fill: '#E2F2FF',
+      currency: 20.0,
+      isDonut: true,
+      isPadded: true
+    },
+    {
+      id: 5,
+      name: 'Tropa',
+      value: 13,
+      fill: '#0098FF',
+      currency: 20.0,
+      isDonut: true,
+      isPadded: true
+    },
+    {
+      id: 6,
+      name: 'Outros',
+      value: 5,
+      fill: '#8CCBFF',
       currency: 20.0,
       isDonut: true,
       isPadded: true
     }
   ];
+
+  const jobsData = [
+    {
+      id_job: 0,
+      client_name: 'Metso',
+      job_name: 'Planejamento',
+      job_status: 'fazendo'
+    },
+    {
+      id_job: 1,
+      client_name: 'Tropa',
+      job_name: 'Planejamento',
+      job_status: 'na fila'
+    },
+    {
+      id_job: 2,
+      client_name: 'Takao',
+      job_name: 'Planejamento',
+      job_status: 'na fila'
+    },
+    {
+      id_job: 3,
+      client_name: 'Iveco',
+      job_name: 'Planejamento',
+      job_status: 'na fila'
+    }
+  ];
+
   return (
     <Container>
       <SectionDefault style={{ display: 'flex', flexDirection: 'column', gap: '32px' }}>
@@ -58,128 +148,127 @@ export default function Dashboard() {
           <CardDataDash data={52} type="warning" description="Equipes" />
         </div>
 
-        <ContainerGroupTable style={{ marginTop: '1rem' }}>
-          <BarChartGrafic />
-          <ChartDonut data={dataDahs} dataKey="value" />
+        <ContainerGroupTable>
+          <BarChartGrafic data={dataStatusAll} title={'Status Geral Jobs'} />
+          <ChartDonut data={dataDahs} title={'Top Clientes'} dataKey="value" />
         </ContainerGroupTable>
 
-        <ContainerGroupTable style={{ marginTop: '1rem' }}>
-          <TableDefault title="titulo da tabela">
+        <ContainerGroupTable>
+          <TableDefault title="Jobs Entregues">
             <thead>
               <tr>
-                <th>Titulo</th>
-                <th>Valor</th>
-                <th>Categoria</th>
-                <th>Data</th>
+                <th>Cliente</th>
+                <th>Job</th>
+                <th>Status</th>
               </tr>
             </thead>
 
             <tbody>
-              <tr>
-                <td>Socios</td>
-                <td>
-                  {new Intl.NumberFormat('pt-BR', {
-                    style: 'currency',
-                    currency: 'BRL'
-                  }).format(15000)}
-                </td>
-                <td>sistemas</td>
-                <td>{new Intl.DateTimeFormat('pt-BR').format(new Date(1300))}</td>
-              </tr>
-              <tr>
-                <td>Clientes</td>
-                <td>
-                  {new Intl.NumberFormat('pt-BR', {
-                    style: 'currency',
-                    currency: 'BRL'
-                  }).format(3200)}
-                </td>
-                <td>sistemas</td>
-                <td>{new Intl.DateTimeFormat('pt-BR').format(new Date(50000))}</td>
-              </tr>
-              <tr>
-                <td>Clientes</td>
-                <td>
-                  {new Intl.NumberFormat('pt-BR', {
-                    style: 'currency',
-                    currency: 'BRL'
-                  }).format(3200)}
-                </td>
-                <td>sistemas</td>
-                <td>{new Intl.DateTimeFormat('pt-BR').format(new Date(50000))}</td>
-              </tr>
-              <tr>
-                <td>Clientes</td>
-                <td>
-                  {new Intl.NumberFormat('pt-BR', {
-                    style: 'currency',
-                    currency: 'BRL'
-                  }).format(3200)}
-                </td>
-                <td>sistemas</td>
-                <td>{new Intl.DateTimeFormat('pt-BR').format(new Date(50000))}</td>
-              </tr>
+              {jobsData.map((row) => (
+                <tr key={row.id_job}>
+                  <td>{row.client_name}</td>
+                  <td>
+                    {row.job_name}
+                    {/* {new Intl.NumberFormat('pt-BR', {
+                      style: 'currency',
+                      currency: 'BRL'
+                    }).format(15000)} */}
+                  </td>
+                  <td
+                    style={{
+                      display: 'flex',
+                      alignItems: 'center',
+                      justifyContent: 'center',
+                      padding: '6px 12px',
+                      height: '49px'
+                    }}
+                  >
+                    <JobStatus
+                      className={
+                        row.job_status === 'fazendo'
+                          ? 'status progress'
+                          : row.job_status === 'na fila'
+                          ? 'status'
+                          : 'status finished'
+                      }
+                    >
+                      {row.job_status}
+                    </JobStatus>
+                  </td>
+                </tr>
+              ))}
             </tbody>
           </TableDefault>
 
-          <TableDefault title="titulo da tabela">
+          <TableDefault title="Jobs Entregues">
             <thead>
               <tr>
-                <th>Titulo</th>
-                <th>Valor</th>
-                <th>Categoria</th>
-                <th>Data</th>
+                <th>Cliente</th>
+                <th>Job</th>
+                <th>Status</th>
               </tr>
             </thead>
 
             <tbody>
-              <tr>
-                <td>Socios</td>
-                <td>
-                  {new Intl.NumberFormat('pt-BR', {
-                    style: 'currency',
-                    currency: 'BRL'
-                  }).format(15000)}
-                </td>
-                <td>sistemas</td>
-                <td>{new Intl.DateTimeFormat('pt-BR').format(new Date(1300))}</td>
-              </tr>
-              <tr>
-                <td>Clientes</td>
-                <td>
-                  {new Intl.NumberFormat('pt-BR', {
-                    style: 'currency',
-                    currency: 'BRL'
-                  }).format(3200)}
-                </td>
-                <td>sistemas</td>
-                <td>{new Intl.DateTimeFormat('pt-BR').format(new Date(50000))}</td>
-              </tr>
-              <tr>
-                <td>Clientes</td>
-                <td>
-                  {new Intl.NumberFormat('pt-BR', {
-                    style: 'currency',
-                    currency: 'BRL'
-                  }).format(3200)}
-                </td>
-                <td>sistemas</td>
-                <td>{new Intl.DateTimeFormat('pt-BR').format(new Date(50000))}</td>
-              </tr>
-              <tr>
-                <td>Clientes</td>
-                <td>
-                  {new Intl.NumberFormat('pt-BR', {
-                    style: 'currency',
-                    currency: 'BRL'
-                  }).format(3200)}
-                </td>
-                <td>sistemas</td>
-                <td>{new Intl.DateTimeFormat('pt-BR').format(new Date(50000))}</td>
-              </tr>
+              {jobsData.map((row) => (
+                <tr key={row.id_job}>
+                  <td>{row.client_name}</td>
+                  <td>
+                    {row.job_name}
+                    {/* {new Intl.NumberFormat('pt-BR', {
+                      style: 'currency',
+                      currency: 'BRL'
+                    }).format(15000)} */}
+                  </td>
+                  <td
+                    style={{
+                      display: 'flex',
+                      alignItems: 'center',
+                      justifyContent: 'center',
+                      padding: '6px 12px',
+                      height: '49px'
+                    }}
+                  >
+                    <JobStatus
+                      className={
+                        row.job_status === 'fazendo'
+                          ? 'status progress'
+                          : row.job_status === 'na fila'
+                          ? 'status'
+                          : 'status finished'
+                      }
+                    >
+                      {row.job_status}
+                    </JobStatus>
+                  </td>
+                </tr>
+              ))}
             </tbody>
           </TableDefault>
         </ContainerGroupTable>
+
+        <ServicePerformance>
+          <div className="title-service">Performance do Atendimento</div>
+
+          <UserCardService>
+            <UserInfo>
+              <div className="user-image" />
+              <div className="user-name">
+                Amanda do Carmo
+                <span>12 clientes</span>
+              </div>
+            </UserInfo>
+
+            <div>grafico</div>
+
+            <MensalReport>
+              <div className="report-bold">Report mensal</div>
+              <div className="report-info">Balanço de ações</div>
+              <div className="report-info">Mídias</div>
+              <div className="report-bold">12 reuniões</div>
+            </MensalReport>
+          </UserCardService>
+        </ServicePerformance>
       </SectionDefault>
     </Container>
   );
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -11,3 +11,112 @@ export const Container = styled.div`
     margin-top: 1rem;
   }
 `;
+
+export const JobStatus = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 12px;
+
+  width: fit-content;
+  height: 24px;
+  border-radius: 35px;
+  background: var(--warning-100);
+
+  color: var(--warning-700);
+  font-size: var(--text-small-sm);
+  font-weight: var(--weight-medium);
+  text-transform: capitalize;
+
+  &.progress {
+    color: var(--primary-700);
+    background: var(--primary-050);
+  }
+
+  &.finished {
+    color: var(--secundary-700);
+    background: var(--secundary-100);
+  }
+`;
+
+export const ServicePerformance = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+
+  background-color: var(--background-primary);
+  padding: 10px 20px 20px 20px;
+
+  border-radius: 10px;
+  box-shadow: 0px 4px 12px 0px rgba(0, 0, 0, 0.05);
+
+  .title-service {
+    color: var(--title-color);
+    font-size: var(--text-small-sm);
+    font-weight: var(--weight-bold);
+  }
+`;
+
+export const UserCardService = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  height: 150px;
+
+  background-color: #f6f7fb;
+  border-radius: 10px;
+
+  padding: 12px;
+`;
+
+export const UserInfo = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 14px;
+
+  .user-image {
+    width: 100px;
+    height: 100px;
+    border-radius: 50%;
+    background-color: purple;
+  }
+
+  .user-name {
+    display: flex;
+    flex-direction: column;
+    gap: 4px;
+
+    min-width: 120px;
+    max-width: 180px;
+
+    color: var(--primary-900);
+    font-size: var(--text-headline-sm);
+    font-weight: var(--weight-bold);
+
+    span {
+      color: var(--title-color);
+      font-size: var(--text-small-sm);
+      font-weight: var(--weight-bold);
+    }
+  }
+`;
+
+export const MensalReport = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+  width: 150px;
+
+  .report-bold {
+    color: var(--title-color);
+    font-size: var(--text-small-lg);
+    font-weight: var(--weight-bold);
+  }
+
+  .report-info {
+    color: var(--gray-500);
+    font-size: var(--text-small-md);
+    font-weight: var(--weight-regular);
+  }
+`;
