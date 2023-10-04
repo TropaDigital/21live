08b982d7003b2f3c1a3a5f1ed49e910bcc5b2e6d
Fino59 <slipdan@hotmail.com>
2023-10-04 18:45:09 -0300

    feat: Update Table Default to turn title optional

--- src/components/TableDefault/index.tsx
+++ src/components/TableDefault/index.tsx
@@ -5,12 +5,17 @@ import { Container, TitleTable } from './styles';
 interface DataTable {
   title: string;
   children: ReactNode;
+  titleSize?: string;
+  titleWeight?: string;
+  titleColor?: string;
 }
 
-export function TableDefault({ title, children }: DataTable) {
+export function TableDefault({ title, children, titleColor, titleSize, titleWeight }: DataTable) {
   return (
     <Container>
-      <TitleTable>{title}</TitleTable>
+      <TitleTable titleColor={titleColor} titleSize={titleSize} titleWeight={titleWeight}>
+        {title}
+      </TitleTable>
 
       <table>{children}</table>
     </Container>
