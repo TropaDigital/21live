d8f9cdc1db0e69cd6dc8f20e83c9bb43b9b9d2d7
Fino59 <slipdan@hotmail.com>
2023-10-18 18:05:09 -0300

    feat: Update Clients page with link to 21App

--- src/pages/Clients/index.tsx
+++ src/pages/Clients/index.tsx
@@ -9,349 +9,158 @@ import { HiOutlineEye } from 'react-icons/hi';
 // Services
 import api from '../../services/api';
 
-// Utils
-import { convertToMilliseconds } from '../../utils/convertToMilliseconds';
-import { useDebounce } from '../../utils/useDebounce';
-
 // Components
 import ButtonDefault from '../../components/Buttons/ButtonDefault';
 import HeaderPage from '../../components/HeaderPage';
-import { InputDefault } from '../../components/Inputs/InputDefault';
-import InputSwitchDefault from '../../components/Inputs/InputSwitchDefault';
-import { SelectDefault } from '../../components/Inputs/SelectDefault';
-import { TableDefault } from '../../components/TableDefault';
-import ProgressBar from '../../components/Ui/ProgressBar';
-import ScrollAreas from '../../components/Ui/ScrollAreas';
-import {
-  ContainerGroupTable,
-  ContentDefault,
-  FieldDefault,
-  FieldGroupFormDefault,
-  FooterModal
-} from '../../components/UiElements/styles';
-
-// Libraries
-import * as Dialog from '@radix-ui/react-dialog';
+import Loader from '../../components/LoaderSpin';
+import { Table } from '../../components/Table';
+import { FilterGroup, TableHead } from '../../components/Table/styles';
 
 // Styles
-import { Container } from './styles';
+import { Container, LogoContainer } from './styles';
 
-interface UserProps {
-  avatar: string;
-  email: string;
+// Hooks
+import { useFetch } from '../../hooks/useFetch';
+import useDebouncedCallback from '../../hooks/useDebounced';
+import { InputDefault } from '../../components/Inputs/InputDefault';
+import Pagination from '../../components/Pagination';
+import ButtonTable from '../../components/Buttons/ButtonTable';
+
+interface TenantProps {
+  bucket: string;
+  colormain: string;
   name: string;
-  tenant_id: number;
-  user_id: number;
-  username: string;
+  slug: string;
+  tenant_id: string;
+  contact_name: string;
+  email: string;
+  meetings: string;
+  reports: string;
+  utils_information: string;
 }
 
 export default function Clients() {
-  const [open, setOpen] = useState(false);
-  const [data, setData] = useState<UserProps[]>([]);
-  const [searchTerm, setSearchTerm] = useState('');
-  const [results, setResults] = useState([] as any);
-  const [isSearching, setSearching] = useState(false);
-  const debouncedSearchTerm = useDebounce(searchTerm, 700);
+  const [searchTerm, setSearchTerm] = useState<string>('');
+  const [search, setSearch] = useState('');
+  const [pageSelected, setPageSelected] = useState<number>(1);
+  const { isLoading, debouncedCallback } = useDebouncedCallback(
+    (search: string) => setSearch(search),
+    700
+  );
+  const { data, pages, isFetching } = useFetch<TenantProps[]>(
+    `tenant?search=${search}&page=${pageSelected}&perPage=15`
+  );
 
-  useEffect(() => {
-    if (debouncedSearchTerm) {
-      setSearching(true);
-      setResults(
-        avatarData.filter((obj: any) => obj.name.toLowerCase().includes(searchTerm.toLowerCase()))
-      );
-      setSearching(false);
-    } else {
-      setResults([]);
-      setSearching(false);
-    }
-  }, [debouncedSearchTerm]);
+  const handleNavigate = (tenantInfo: TenantProps) => {
+    const linkEdit = {
+      slug: tenantInfo.slug,
+      id: tenantInfo.tenant_id
+    };
 
-  useEffect(() => {
-    getCustomers();
-  }, []);
+    window.location.href = `https://app.21live.com.br/${linkEdit.slug}/tenants/edit/${linkEdit.id}`;
 
-  async function getCustomers() {
-    const response = await api.get('tenant');
-
-    setData(response.data.result);
-  }
-
-  const optionsCoffe = [
-    {
-      id: 1,
-      name: 'Cagfé Melita'
-    },
-    {
-      id: 2,
-      name: 'Café Pelé'
-    },
-    {
-      id: 3,
-      name: 'Café Tres corações'
-    }
-  ];
-
-  const avatarData = [
-    {
-      id: 1,
-      name: 'João',
-      url: 'http://userimage.png',
-      isOnline: true
-    },
-    {
-      id: 2,
-      name: 'Maria',
-      url: 'http://userimage.png',
-      isOnline: false
-    },
-    {
-      id: 3,
-      name: 'Pedro',
-      url: 'http://userimage.png',
-      isOnline: true
-    },
-    {
-      id: 4,
-      name: 'Bete',
-      url: 'http://userimage.png',
-      isOnline: false
-    },
-    {
-      id: 5,
-      name: 'Mario',
-      url: 'http://userimage.png',
-      isOnline: true
-    },
-    {
-      id: 6,
-      name: 'Pedro',
-      url: 'http://userimage.png',
-      isOnline: true
-    },
-    {
-      id: 7,
-      name: 'Bete',
-      url: 'http://userimage.png',
-      isOnline: false
-    },
-    {
-      id: 8,
-      name: 'Mario',
-      url: 'http://userimage.png',
-      isOnline: true
-    }
-  ];
-
-  const [formDataClient, setFormDataClient] = useState({
-    name: '',
-    code: ''
-  });
-
-  const [formData, setFormData] = useState([
-    {
-      id: useId(),
-      client: 'João',
-      isActive: true,
-      activityHours: '05:20:24',
-      projects: '5',
-      hoursinvested: '10:00:00',
-      hoursLeft: '07:00:00'
-    },
-    {
-      id: useId(),
-      client: 'Maria',
-      isActive: false,
-      activityHours: '02:30:24',
-      projects: '4',
-      hoursinvested: '10:00:00',
-      hoursLeft: '04:00:00'
-    },
-    {
-      id: useId(),
-      client: 'Pedro',
-      isActive: true,
-      activityHours: '04:20:24',
-      projects: '2',
-      hoursinvested: '10:00:00',
-      hoursLeft: '01:00:00'
-    },
-    {
-      id: useId(),
-      client: 'Raul',
-      isActive: true,
-      activityHours: '15:20:24',
-      projects: '1',
-      hoursinvested: '10:00:00',
-      hoursLeft: '05:00:00'
-    }
-  ]);
-
-  function changePosition(arr: any, from: any, to: any) {
-    arr.splice(to, 0, arr.splice(from, 1)[0]);
-    return arr;
-  }
-
-  const handleInputChange = (name: string, event: React.ChangeEvent<HTMLInputElement>) => {
-    setFormDataClient({ ...formDataClient, [name]: event.target.value });
+    // console.log('log do navigate', tenantInfo);
+    // console.log(
+    //   'log do link para editar:',
+    //   `https://app.21live.com.br/${linkEdit.slug}/tenants/edit/${linkEdit.id}`
+    // );
   };
-
-  const handleSwitchChange = (position: any, id: any, e: React.ChangeEvent<HTMLInputElement>) => {
-    const filterData = formData.filter((obj) => obj.id === id);
-    filterData[0].isActive = e.target.checked;
-
-    const filterFormData = formData.filter((obj) => obj.id !== id);
-
-    const positionArray = changePosition(filterData.concat(filterFormData), 0, position);
-    setFormData(positionArray);
-  };
-
-  const handleOnSubmit = useCallback(async (event: any) => {
-    try {
-      event.preventDefault();
-
-      // Inserir lógica
-    } catch (e: any) {
-      console.log(e);
-    }
-    // Exibir erro
-  }, []);
 
   return (
     <Container>
       <HeaderPage title="Clientes">
-        <ButtonDefault typeButton="success" onClick={() => setOpen(!open)}>
+        <ButtonDefault
+          typeButton="success"
+          onClick={() => alert('Falta decidir o caminho aqui!!!')}
+        >
           <BiPlus color="#fff" />
           Novo Cliente
         </ButtonDefault>
       </HeaderPage>
 
-      <ContentDefault>
-        <FieldGroupFormDefault>
-          <SelectDefault
-            name="filter"
-            label="Filtro"
-            placeholder="Todos"
-            onChange={(event) => console.log('filter', event)}
-          >
-            {optionsCoffe.map((row) => (
-              <option key={row.id} value={row.id}>
-                {row.name}
-              </option>
-            ))}
-          </SelectDefault>
+      {isFetching && <Loader />}
 
-          <InputDefault
-            label="Busca"
-            name="search"
-            placeholder="Buscar..."
-            onChange={(event) => setSearchTerm(event.target.value)}
-            icon={BiSearchAlt}
-          />
-        </FieldGroupFormDefault>
-        <ButtonDefault style={{ marginTop: '24px', float: 'right' }} typeButton="info">
-          <BiFilter />
-        </ButtonDefault>
-      </ContentDefault>
+      {!isFetching && (
+        <Table>
+          <TableHead>
+            <div className="groupTable">
+              <h2>Lista de clientes</h2>
+            </div>
+          </TableHead>
 
-      <ContainerGroupTable style={{ marginTop: '1rem' }}>
-        <ScrollAreas>
-          <TableDefault title="Clientes">
+          <FilterGroup>
+            <InputDefault
+              label=""
+              name="search"
+              placeholder="Buscar..."
+              onChange={(event) => {
+                setSearchTerm(event.target.value);
+                debouncedCallback(event.target.value);
+              }}
+              value={searchTerm}
+              icon={BiSearchAlt}
+              isLoading={isLoading}
+              className="search-field"
+            />
+          </FilterGroup>
+
+          <table>
             <thead>
-              <tr style={{ whiteSpace: 'nowrap' }}>
-                <th>Código</th>
-                <th>Clientes</th>
-                <th>Ativo</th>
-                <th>Atividade</th>
-                <th>Projetos</th>
-                <th>Horas investidas</th>
-                <th>Horas restantes</th>
-                <th style={{ display: 'grid', placeItems: 'center' }}>-</th>
+              <tr>
+                <th>ID</th>
+                <th>Logo</th>
+                <th>Nome</th>
+                <th>Endereço</th>
+                <th style={{ display: 'grid', placeItems: 'center', color: '#F9FAFB' }}>-</th>
               </tr>
             </thead>
 
             <tbody>
-              {formData.map((row, key) => (
-                <tr key={row.id}>
-                  <td>{row.id}</td>
-                  <td>{row.client}</td>
-                  <td>
-                    <InputSwitchDefault
-                      isChecked={row.isActive}
-                      onChange={(e) => handleSwitchChange(key, row.id, e)}
-                      disabled
-                    />
+              {data?.map((row: TenantProps) => (
+                <tr key={row.tenant_id}>
+                  <td>#{row.tenant_id}</td>
+                  <td style={{ cursor: 'pointer' }} onClick={() => handleNavigate(row)}>
+                    <LogoContainer>
+                      <div
+                        className="client-image"
+                        style={{
+                          backgroundColor: `#${row.colormain}`,
+                          backgroundImage: `url(https://${row.bucket}.s3.amazonaws.com/tenant/logo.png)`
+                        }}
+                      />
+                    </LogoContainer>
                   </td>
-                  <td
-                    style={{
-                      padding: '14px',
-                      width: '220px',
-                      textAlign: 'left'
-                    }}
-                  >
-                    {row.activityHours}
-                    <ProgressBar
-                      totalHours={convertToMilliseconds(row.hoursinvested)}
-                      restHours={convertToMilliseconds(row.hoursLeft)}
-                    />
+                  <td style={{ cursor: 'pointer' }} onClick={() => handleNavigate(row)}>
+                    {row.name}
                   </td>
-                  <td>{row.projects}</td>
-                  <td>{row.hoursinvested}</td>
-                  <td>{row.hoursLeft}</td>
+                  <td style={{ cursor: 'pointer' }} onClick={() => handleNavigate(row)}>
+                    /{row.slug}
+                  </td>
                   <td>
                     <div className="fieldTableClients">
-                      <ButtonDefault typeButton="warning">
-                        <HiOutlineEye />
-                      </ButtonDefault>
-                      <ButtonDefault typeButton="info">
-                        <BiEdit />
-                      </ButtonDefault>
+                      <ButtonTable typeButton="edit" onClick={() => handleNavigate(row)} />
                     </div>
                   </td>
                 </tr>
               ))}
             </tbody>
-          </TableDefault>
-        </ScrollAreas>
-      </ContainerGroupTable>
 
-      <Dialog.Root open={open} onOpenChange={setOpen}>
-        <Dialog.Portal>
-          <Dialog.Overlay className="DialogOverlay" />
-          <Dialog.Content className="DialogContent">
-            <Dialog.Title className="DialogTitle">Novo Cliente</Dialog.Title>
-            <form onSubmit={handleOnSubmit}>
-              <FieldDefault>
-                <InputDefault
-                  label="Nome do cliente"
-                  name="nome"
-                  onChange={(event) => handleInputChange('name', event)}
-                />
-              </FieldDefault>
-              <FieldDefault>
-                <InputDefault
-                  label="Código do cliente"
-                  name="code"
-                  onChange={(event) => handleInputChange('code', event)}
-                  required
-                />
-              </FieldDefault>
-              <FooterModal style={{ justifyContent: 'flex-end', gap: '16px' }}>
-                <ButtonDefault typeButton="dark" isOutline onClick={() => setOpen(!open)}>
-                  Descartar
-                </ButtonDefault>
-                <ButtonDefault typeButton="primary" isOutline type="submit">
-                  Salvar
-                </ButtonDefault>
-              </FooterModal>
-            </form>
-            <Dialog.Close asChild>
-              <button className="IconButton" aria-label="Close">
-                <BiX size={30} color="#6C757D" />
-              </button>
-            </Dialog.Close>
-          </Dialog.Content>
-        </Dialog.Portal>
-      </Dialog.Root>
+            <tfoot>
+              <tr>
+                <td colSpan={100}>
+                  <Pagination
+                    total={pages.total}
+                    perPage={pages.perPage}
+                    currentPage={pageSelected}
+                    lastPage={pages.lastPage}
+                    onClickPage={(e) => setPageSelected(e)}
+                  />
+                </td>
+              </tr>
+            </tfoot>
+          </table>
+        </Table>
+      )}
     </Container>
   );
 }
--- src/pages/Clients/styles.ts
+++ src/pages/Clients/styles.ts
@@ -4,3 +4,25 @@ export const Container = styled.div`
   display: flex;
   flex-direction: column;
 `;
+
+export const LogoContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  width: 50px;
+  height: 50px;
+  border-radius: 100%;
+  border: 1px solid rgba(0, 0, 0, 0.7);
+  object-fit: contain;
+  overflow: hidden;
+
+  .client-image {
+    width: 50px;
+    height: 50px;
+
+    background-position: center;
+    background-size: contain;
+    background-repeat: no-repeat;
+  }
+`;
