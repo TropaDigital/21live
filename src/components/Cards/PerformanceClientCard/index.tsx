18d17ec82c4cf35b8ec4a6f5c33fa394b6863ed8
Fino59 <slipdan@hotmail.com>
2023-10-20 18:31:12 -0300

    feat: Create Performance Client Card component

---
+++ src/components/Cards/PerformanceClientCard/index.tsx
@@ -0,0 +1,116 @@
+import BarChartUser from '../../GraphicsChart/BarChartUser';
+import { ContainerCard, GraphicsCard, JobStatusTable, MensalReport } from './styles';
+
+interface ClientPerformance {
+  data_chart?: ChartDataProps;
+  data_table?: TableDataProps[];
+  mensal_report?: MensalReportProps;
+}
+
+export interface ChartDataProps {
+  client_name: string;
+  graphics: ChartGraphic[];
+}
+
+interface ChartGraphic {
+  name: string;
+  pv: number;
+  fill: string;
+}
+
+interface TableDataProps {
+  job_name: string;
+  status_job: string;
+}
+
+interface MensalReportProps {
+  reunions: number;
+  principalTask: string;
+  secondaryTask: string;
+}
+
+export default function PerformanceClientCard({
+  data_chart,
+  data_table,
+  mensal_report
+}: ClientPerformance) {
+  return (
+    <ContainerCard>
+      <GraphicsCard>
+        <div className="graphicTitle">{data_chart?.client_name}</div>
+
+        <BarChartUser data={data_chart?.graphics} height={'160px'} />
+      </GraphicsCard>
+
+      <JobStatusTable>
+        <table>
+          <thead>
+            <tr>
+              <th>JOB</th>
+              <th>STATUS</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data_table?.map((row, index: number) => (
+              <tr key={index}>
+                <td>{row.job_name}</td>
+                <td>
+                  <div
+                    className={
+                      row.status_job === 'creation'
+                        ? 'status progress'
+                        : row.status_job === 'waiting'
+                        ? 'status'
+                        : row.status_job === 'finished'
+                        ? 'status finished'
+                        : 'status'
+                    }
+                  >
+                    {row.status_job === 'creation'
+                      ? 'Criação'
+                      : row.status_job === 'waiting'
+                      ? 'Aguardando aprovação'
+                      : row.status_job === 'finished'
+                      ? 'Concluída'
+                      : 'Pendente'}
+                  </div>
+                </td>
+              </tr>
+            ))}
+            {/* <tr>
+              <td>Planejamento</td>
+              <td>
+                <div className="status">Pendente</div>
+              </td>
+            </tr>
+            <tr>
+              <td>Planejamento</td>
+              <td>
+                <div className="status">Aguardando aprovação</div>
+              </td>
+            </tr>
+            <tr>
+              <td>Planejamento</td>
+              <td>
+                <div className="status progress">Criação</div>
+              </td>
+            </tr> */}
+          </tbody>
+        </table>
+      </JobStatusTable>
+
+      <MensalReport>
+        <div className="report-bold">Report mensal</div>
+        <div className="report-info">
+          {mensal_report?.principalTask}
+          <div className="overflow-text">{mensal_report?.principalTask}</div>
+        </div>
+        <div className="report-info">
+          {mensal_report?.secondaryTask}
+          <div className="overflow-text">{mensal_report?.secondaryTask}</div>
+        </div>
+        <div className="report-bold">{mensal_report?.reunions} reuniões</div>
+      </MensalReport>
+    </ContainerCard>
+  );
+}
