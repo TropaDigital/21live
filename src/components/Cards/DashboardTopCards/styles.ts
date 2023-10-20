b217bf9f91869a170da003d3ea626da88423a180
Fino59 <slipdan@hotmail.com>
2023-10-20 18:30:42 -0300

    feat: Create Dashboard Top Cards component

---
+++ src/components/Cards/DashboardTopCards/styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const WrapperCards = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(220px, 1fr));
+  gap: 1rem;
+  margin-top: 1rem;
+`;
