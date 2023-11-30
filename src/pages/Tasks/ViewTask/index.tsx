fb5df35d02b23142272f69b5cbd65bc98b898a30
Fino59 <slipdan@hotmail.com>
2023-11-30 17:34:52 -0300

    fix: Update View Task page correctly function

--- src/pages/Tasks/ViewTask/index.tsx
+++ src/pages/Tasks/ViewTask/index.tsx
@@ -147,7 +147,7 @@ export default function ViewTask() {
       try {
         setLoading(true);
         const response = await api.get(`/tasks/${id}`);
-        console.log('log do response get task', response.data.result);
+        // console.log('log do response get task', response.data.result);
 
         if (response.data.result.length > 0) {
           setDataTask(response.data.result[0]);
