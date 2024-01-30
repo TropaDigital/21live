7f52f4a838c4c9e3a2db4dbf2f8a32c2cad11839
Fino59 <slipdan@hotmail.com>
2024-01-30 18:02:18 -0300

    fix: Update to correct upload file name

--- src/pages/Products/WorkingProduct/index.tsx
+++ src/pages/Products/WorkingProduct/index.tsx
@@ -507,6 +507,7 @@ export default function WorkingProduct({
       const uploadInfos = {
         task_id: taskId,
         file_name: uploadedFiles[0].file_name,
+        original_name: uploadedFiles[0].original_name,
         size: uploadedFiles[0].size,
         key: uploadedFiles[0].key,
         bucket: uploadedFiles[0].bucket,
@@ -548,6 +549,7 @@ export default function WorkingProduct({
       const uploadInfos = {
         task_id: taskId,
         file_name: uploadedFiles[0].file_name,
+        original_name: uploadedFiles[0].original_name,
         size: uploadedFiles[0].size,
         key: uploadedFiles[0].key,
         bucket: uploadedFiles[0].bucket,
@@ -596,6 +598,7 @@ export default function WorkingProduct({
       const uploadInfos = {
         task_id: taskId,
         file_name: uploadedFiles[0].file_name,
+        original_name: uploadedFiles[0].original_name,
         size: uploadedFiles[0].size,
         key: uploadedFiles[0].key,
         bucket: uploadedFiles[0].bucket,
